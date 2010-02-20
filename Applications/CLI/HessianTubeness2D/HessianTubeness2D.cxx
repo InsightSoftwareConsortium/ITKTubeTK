67b9da45318fa6bce3b4dbeabceaa54623578633
Stephen R. Aylward <stephen.aylward@kitware.com>
2010-02-20 10:16:33 -0500

    ENH: Updated HessianTubeness2D to use simplified CMakeLists.txt file and CLI helper function

--- Applications/CLI/HessianTubeness2D/HessianTubeness2D.cxx
+++ Applications/CLI/HessianTubeness2D/HessianTubeness2D.cxx
@@ -1,3 +1,26 @@
+/*=========================================================================
+
+Library:   TubeTK
+
+Copyright 2010 Kitware Inc. 28 Corporate Drive,
+Clifton Park, NY, 12065, USA.
+
+All rights reserved. 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=========================================================================*/
+
 #if defined(_MSC_VER)
 #pragma warning ( disable : 4786 )
 #endif
@@ -6,24 +29,33 @@
 #define ITK_LEAN_AND_MEAN
 #endif
 
+// Must declare a forward declaration of DoIt before tubeCLIHelperFunctions
+template< class pixelT, unsigned int dimensionT >
+int DoIt( int argc, char **argv );
+
+// Must include CLP file before tubeCLIHelperFunctions
+#include "HessianTubeness2DCLP.h"
+
+#include "tubeCLIHelperFunctions.h"
+
 #include "itkOrientedImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-
 #include "itkTubeEnhancingDiffusion2DImageFilter.h"
 
-#include "HessianTubeness2DCLP.h"
-
-int main( int argc, char * argv[] )
-{
+template< class pixelT, unsigned int dimensionT >
+int DoIt( int argc, char **argv )
+  {
   PARSE_ARGS;
 
-  typedef float                                                   PixelType;
-  typedef itk::Image< PixelType,  2 >                             ImageType;
-  typedef itk::ImageFileReader< ImageType >                       ReaderType;
-  typedef itk::TubeEnhancingDiffusion2DImageFilter< PixelType > FilterType;
+  typedef pixelT                                           PixelType;
+  typedef itk::Image< PixelType,  dimensionT  >            ImageType;
+  typedef itk::ImageFileReader< ImageType >                ReaderType;
+  typedef itk::TubeEnhancingDiffusion2DImageFilter< PixelType,
+                                                    dimensionT  >
+                                                           FilterType;
   
-  ReaderType::Pointer reader = ReaderType::New();
+  typename ReaderType::Pointer reader = ReaderType::New();
 
   //read input image  
   reader->SetFileName( inputVolume.c_str() );
@@ -39,7 +71,7 @@ int main( int argc, char * argv[] )
     return EXIT_FAILURE;
     }
   
-  FilterType::Pointer filter = NULL;
+  typename FilterType::Pointer filter = NULL;
   filter = FilterType::New();
   
   // set input image
@@ -69,9 +101,9 @@ int main( int argc, char * argv[] )
 
   // compute vesselness image
   filter->Update();
-  
+
   typedef itk::ImageFileWriter< ImageType  >   ImageWriterType;
-  ImageWriterType::Pointer writer = ImageWriterType::New();
+  typename ImageWriterType::Pointer writer = ImageWriterType::New();
 
   writer->SetFileName( outputVolume.c_str() );
   writer->SetInput ( filter->GetOutput() );
@@ -85,7 +117,14 @@ int main( int argc, char * argv[] )
     std::cerr << "Exception caught: " << err << std::endl;
     return EXIT_FAILURE;
     }
-  
+
   return EXIT_SUCCESS;
-}
+  }
+
+int main( int argc, char * argv[] )
+  {   
+  PARSE_ARGS;  
+
+  return tube::ParseArgsAndCallDoIt( inputVolume, argc, argv );
+  }
 
