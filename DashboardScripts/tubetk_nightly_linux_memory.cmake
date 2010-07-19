f7c33186c103a51ed0de8e4527fc2797cf968eee
Stephen R. Aylward <stephen.aylward@kitware.com>
2010-07-19 01:00:22 -0400

    ENH: Revising nightly build scripts

--- DashboardScripts/tubetk_nightly_linux_memory.cmake
+++ DashboardScripts/tubetk_nightly_linux_memory.cmake
@@ -21,7 +21,7 @@
 # 
 ##############################################################################
 
-cmake_minimum_required( VERSION 2.6 )
+cmake_minimum_required(VERSION 2.6)
 
 include( ${CTEST_SCRIPT_DIRECTORY}/../../tubetk_config.cmake )
 
@@ -38,31 +38,21 @@ set( CTEST_BINARY_DIRECTORY "${SITE_BINARY_DIR}" )
 
 set( ENV{DISPLAY} ":0" )
 
-set( CTEST_UPDATE_COMMAND "${SITE_UPDATE_COMMAND}" )
 set( CTEST_BUILD_COMMAND "${SITE_MAKE_COMMAND}" )
 set( CTEST_CMAKE_COMMAND "${SITE_CMAKE_COMMAND}" )
 set( CTEST_QMAKE_COMMAND "${SITE_QMAKE_COMMAND}" )
+set( CTEST_UPDATE_COMMAND "${SITE_UPDATE_COMMAND}" )
+set( CTEST_COVERAGE_COMMAND "${SITE_COVERAGE_COMMAND}" )
 
+set( CTEST_MEMORYCHECK_COMMAND "${SITE_MEMORYCHECK_COMMAND}" )
+set( CTEST_MEMORYCHECK_SUPPRESSIONS_FILE
+   "${CTEST_SCRIPT_DIRECTORY}/tubetk_valgrind_supression.txt" )
+set( CTEST_MEMORYCHECK_COMMAND_OPTIONS "${MEMORYCHECK_OPTIONS}" )
 
-# Write initial cache.
-file( WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "
-  BUILD_TESTING:BOOL=ON
-  BUILD_SHARED_LIBS:BOOL=ON
-  CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
-  SITE:STRING=${CTEST_SITE}
-  BUILDNAME:STRING=${CTEST_BUILD_NAME}
-  MAKECOMMAND:STRING=${CTEST_BUILD_COMMAND}
-  QT_QMAKE_EXECUTABLE:FILEPATH=${CTEST_QMAKE_COMMAND}
-  CMAKE_CXX_FLAGS:STRING=${SITE_CXX_FLAGS}
-  CMAKE_C_FLAGS:STRING=${SITE_C_FLAGS}
-  " )
- 
-ctest_start( Nightly )
-ctest_update( SOURCE "${CTEST_SOURCE_DIRECTORY}" )
-ctest_configure( BUILD "${CTEST_BINARY_DIRECTORY}" )
+message( "Running memory check..." )
+
+ctest_start( Continuous )
 ctest_read_custom_files( "${CTEST_BINARY_DIRECTORY}" )
-ctest_build( BUILD "${CTEST_BINARY_DIRECTORY}" )
-ctest_submit()
-ctest_test( BUILD "${CTEST_BINARY_DIRECTORY}/TubeTK-Build" )
+ctest_memcheck( BUILD "${CTEST_BINARY_DIRECTORY}" )
 ctest_submit()
 
