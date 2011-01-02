da43fa4946e38cc45f60154f03c7a625d8f33adf
Stephen R. Aylward <stephen.aylward@kitware.com>
2011-01-01 23:37:22 -0500

    COMP: Removed debugging output and upgraded to requiring CMake2.8

--- DashboardScripts/tubetk_coverage.cmake
+++ DashboardScripts/tubetk_coverage.cmake
@@ -1,27 +1,27 @@
 ##############################################################################
-# 
+#
 # Library:   TubeTK
-# 
+#
 # Copyright 2010 Kitware Inc. 28 Corporate Drive,
 # Clifton Park, NY, 12065, USA.
-# 
-# All rights reserved. 
-# 
+#
+# All rights reserved.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #       http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 ##############################################################################
 
-cmake_minimum_required(VERSION 2.6)
+cmake_minimum_required(VERSION 2.8)
 
 include( ${CTEST_SCRIPT_DIRECTORY}/../../tubetk_config.cmake )
 
@@ -31,8 +31,6 @@ set( SCRIPT_TubeTK_USE_SUPERBUILD OFF )
 
 include( ${CTEST_SCRIPT_DIRECTORY}/tubetk_cmakecache.cmake )
 
-MESSAGE( "CTEST_BINARY_DIRECTORY = ${CTEST_BINARY_DIRECTORY}" )
-
 ctest_start( "$ENV{TUBETK_RUN_MODEL}" )
 ctest_configure( BUILD "${CTEST_BINARY_DIRECTORY}" )
 ctest_read_custom_files( "${CTEST_BINARY_DIRECTORY}" )
@@ -40,4 +38,3 @@ ctest_build( BUILD "${CTEST_BINARY_DIRECTORY}" )
 ctest_test( BUILD "${CTEST_BINARY_DIRECTORY}" )
 ctest_coverage( BUILD "${CTEST_BINARY_DIRECTORY}" )
 ctest_submit()
-
