78cdf344ffa016c02f7bbc8ed0b6c3f04b9e6a47
Nathan Taylor <nathan.taylor@kitware.com>
2011-08-18 14:46:18 -0400

    ENH: Added new tests for variations on VotingResample use cases

--- Base/Numerics/Testing/itkVotingResampleImageFunctionTest.cxx
+++ Base/Numerics/Testing/itkVotingResampleImageFunctionTest.cxx
@@ -58,17 +58,34 @@ int itkVotingResampleImageFunctionTest(int argc, char* argv[] )
 
   // Create and configure an image
   ImageReaderType::Pointer imReader = ImageReaderType::New();
-  imReader->SetFileName( argv[1] );
+  imReader->SetFileName( argv[2] );
   imReader->Update();
 
-  ImageSizeType size = imReader->GetOutput()
-    ->GetLargestPossibleRegion().GetSize();
-  size[0] *= 2;
-  size[1] *= 2;
+  int testNumber = atoi( argv[1] );
 
+  double scale = 1;
+  ImageSizeType size = imReader->GetOutput()->GetLargestPossibleRegion().GetSize();
+  if( testNumber == 0 )
+    {
+    scale = 2;
+    size[0] *= 2;
+    size[1] *= 2;
+    }
+  else if( testNumber == 1 )
+    {
+    scale = 1.5;
+    size[0] *= 1.5;
+    size[1] *= 1.5;
+    }
+  else if( testNumber == 2 )
+    {
+    scale = 0.5;
+    size[0] *= 0.5;
+    size[1] *= 0.5;
+    }
   // Create an affine transformation
   AffineTransformType::Pointer aff = AffineTransformType::New();
-  aff->Scale( 0.5 );
+  aff->Scale( 1.0/scale );
 
   // Create a linear interpolation image function
   InterpolatorType::Pointer interp = InterpolatorType::New();
@@ -98,7 +115,7 @@ int itkVotingResampleImageFunctionTest(int argc, char* argv[] )
   resample->Update();
 
   ImageWriterType::Pointer imWriter = ImageWriterType::New();
-  imWriter->SetFileName( argv[2] );
+  imWriter->SetFileName( argv[3] );
   imWriter->SetInput( resample->GetOutput() );
   imWriter->Update();
 
