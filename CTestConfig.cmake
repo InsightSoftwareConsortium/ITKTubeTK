b737f9a6f6d12ccdac707731e54d78a5e5c05068
Stephen R. Aylward <stephen.aylward@kitware.com>
2012-04-13 12:24:11 -0400

    ENH: Pointing to new CDash site

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,9 +5,9 @@
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
 set(CTEST_PROJECT_NAME "TubeTK")
-set(CTEST_NIGHTLY_START_TIME "24:00:00 EDT")
+set(CTEST_NIGHTLY_START_TIME "23:59:00 EDT")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.kitware.com")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=TubeTK")
+set(CTEST_DROP_SITE "open.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=TubeTK")
 set(CTEST_DROP_SITE_CDASH TRUE)
