9ca4ee78698b4c5f6bc4e6bb7040938e00f2a259
andinet enquobahrie <andinet.enqu@kitware.com>
2012-08-27 13:01:47 -0400

    COMP: Fix unused variable warnings

--- Base/Filtering/Testing/itkSheetnessMeasureImageFilterTest2.cxx
+++ Base/Filtering/Testing/itkSheetnessMeasureImageFilterTest2.cxx
@@ -203,14 +203,12 @@ int itkSheetnessMeasureImageFilterTest2(int argc ,char* argv[] )
 
     // Find the largest eigenvalue
     double largest = vnl_math_abs( eigenValue[0] );
-    double Lambda3 = eigenValue[0];
     unsigned int largestEigenValueIndex=0;
 
     for ( unsigned int i=1; i <=2; i++ )
       {
       if (  vnl_math_abs( eigenValue[i] > largest ) )
         {
-        Lambda3 = eigenValue[i];
         largest = vnl_math_abs( eigenValue[i] );
         largestEigenValueIndex = i;
         }
