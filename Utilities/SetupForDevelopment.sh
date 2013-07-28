5c0cdbc962fa793336a519922b18df125d4dfb81
Stephen R. Aylward <stephen.aylward@kitware.com>
2013-07-28 17:11:52 -0400

    BUG: Update SetupForDevelopment.sh and associated files for Github

--- Utilities/SetupForDevelopment.sh
+++ Utilities/SetupForDevelopment.sh
@@ -26,12 +26,6 @@
 cd "${BASH_SOURCE%/*}/.." &&
 Utilities/GitSetup/setup-user && echo &&
 Utilities/GitSetup/setup-hooks && echo &&
-Utilities/GitSetup/setup-aliases && echo &&
-Utilities/GitSetup/setup-stage && echo &&
-(Utilities/GitSetup/setup-ssh ||
- echo 'Failed to setup SSH.  Run this again to retry.') && echo &&
-(Utilities/GitSetup/setup-gerrit ||
- echo 'Failed to setup Gerrit.  Run this again to retry.') && echo &&
 Utilities/GitSetup/tips
 
 # Rebase master by default
