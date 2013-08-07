6ab8e39a1ff766e3fc8b5802171d01ec2b294149
Matt McCormick <matt.mccormick@kitware.com>
2013-08-06 23:15:37 -0400

    BUG: Use the parameter progression file stored in json.
    Change-Id: I85d6469960f921b488ea5d523f6b6d9f8307dd8f

--- Applications/RegisterImageToTubesUsingRigidTransform/RegisterImageToTubesUsingRigidTransformTuner.py
+++ Applications/RegisterImageToTubesUsingRigidTransform/RegisterImageToTubesUsingRigidTransformTuner.py
@@ -8,19 +8,20 @@ import json
 import subprocess
 import sys
 
+
 def run_analysis(config, config_file):
     io_params = config['ParameterGroups'][0]['Parameters']
+    print(io_params[2]['Value'])
     subprocess.check_call([config['Executables']['Analysis'],
-        '--parameterstorestore', config_file.name,
-        io_params[0]['Value'],
-        io_params[1]['Value'],
-        io_params[2]['Value'],
-        '--parameterProgression', io_params[2]['Value']])
+                          '--parameterstorestore', config_file.name,
+                          io_params[0]['Value'],
+                          io_params[1]['Value'],
+                          io_params[2]['Value']])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('configuration', type=argparse.FileType('r'),
-            help='Configuration file for tuning analysis')
+                        help='Configuration file for tuning analysis')
     args = parser.parse_args()
     config_file = args.configuration
 
