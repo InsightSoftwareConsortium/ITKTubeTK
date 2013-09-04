6b84069fa26c89d5e94ce25ef14b88c01a8308ae
Matt McCormick <matt.mccormick@kitware.com>
2013-09-04 00:28:00 +0000

    ENH: Remove duplicate points when sub-sampling tubes.

--- Base/Registration/Testing/itktubeTubeAngleOfIncidenceWeightFunctionTest.cxx
+++ Base/Registration/Testing/itktubeTubeAngleOfIncidenceWeightFunctionTest.cxx
@@ -176,9 +176,9 @@ int itktubeTubeAngleOfIncidenceWeightFunctionTest( int argc, char * argv[] )
 
   // Regression test on a few points
   WeightType tolerance = 0.01;
-  if( vnl_math_abs( weights[3] - 0.996318 ) > tolerance ||
-      vnl_math_abs( weights[4] - 0.818436 ) > tolerance ||
-      vnl_math_abs( weights[5] - 0.909169 ) > tolerance )
+  if( vnl_math_abs( weights[3] - 0.818436 ) > tolerance ||
+      vnl_math_abs( weights[4] - 0.909169 ) > tolerance ||
+      vnl_math_abs( weights[5] - 0.951959 ) > tolerance )
     {
     std::cerr << "Did not generate the expected weights." << std::endl;
     return EXIT_FAILURE;
