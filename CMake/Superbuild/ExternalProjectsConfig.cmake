364ef911e38b8a6bc7a322d180b1cc4e39d91e2a
Jamie Snape <jamie.snape@kitware.com>
2013-09-05 13:58:38 -0400

    ENH: Use stable source tar ball for Cppcheck

--- CMake/Superbuild/ExternalProjectsConfig.cmake
+++ CMake/Superbuild/ExternalProjectsConfig.cmake
@@ -24,9 +24,8 @@
 # See https://github.com/TubeTK/TubeTK/wiki/Dependencies
 
 # Cppcheck version 1.61
-set( Cppcheck_GIT_REPOSITORY
-  ${github_protocol}://github.com/danmar/cppcheck.git )
-set( Cppcheck_GIT_TAG 9e16abeb4275f01b2041972a2edbd6ad46d945c1 )
+set( Cppcheck_URL https://github.com/danmar/cppcheck/archive/1.61.tar.gz )
+set( Cppcheck_URL_MD5 362b6f8cf907262a7e3869b99183d734 )
 
 # Common Toolkit snapshot 2013-08-20 16:21:07
 set( CTK_GIT_REPOSITORY ${github_protocol}://github.com/commontk/CTK.git )
