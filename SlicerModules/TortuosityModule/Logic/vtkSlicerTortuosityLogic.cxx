5d2f1ddb4752952e87e722dc5422af59a691f18b
Johan Andruejol <johan.andruejol@kitware.com>
2014-08-04 18:45:26 -0400

    Move logic's tortuosity computation to a TubeTK filter - 2/2
    Use the new filter in the logic.

--- SlicerModules/TortuosityModule/Logic/vtkSlicerTortuosityLogic.cxx
+++ SlicerModules/TortuosityModule/Logic/vtkSlicerTortuosityLogic.cxx
@@ -25,6 +25,7 @@ limitations under the License.
 
 // ITK includes
 #include "itkVesselTubeSpatialObject.h"
+#include "itktubeTortuositySpatialObjectFilter.h"
 
 // Spatial object includes
 #include "vtkMRMLSpatialObjectsNode.h"
@@ -209,48 +210,6 @@ bool vtkSlicerTortuosityLogic
 }
 
 //------------------------------------------------------------------------------
-namespace
-{
-template<typename PointType> void CopyPoints(PointType* point, double* p)
-{
-  for (int i = 0; i < 3; ++i)
-    {
-    p[i] = static_cast<double>(point->GetPosition()[i]);
-    }
-}
-
-void CopyVector3(double* from, double* to)
-{
-  for (int i = 0; i < 3; ++i)
-    {
-    to[i] = from[i];
-    }
-}
-
-void InitVector3(double* v)
-{
-  for (int i = 0; i < 3; ++i)
-    {
-    v[i] = 0.0;
-    }
-}
-
-double SafeAcos(double x)
-{
-  if (x < -1.0)
-    {
-    x = -1.0;
-    }
-  else if (x > 1.0)
-    {
-    x = 1.0;
-    }
-  return acos(x);
-}
-
-} // end namespace
-
-//------------------------------------------------------------------------------
 bool vtkSlicerTortuosityLogic
 ::RunMetrics(vtkMRMLSpatialObjectsNode* node, int flag)
 {
@@ -258,8 +217,6 @@ bool vtkSlicerTortuosityLogic
     {
     return false;
     }
-
-  bool noProblem = true;
 
   // 1 - Get the metric arrays
   vtkDoubleArray* dm = this->GetDistanceMetricArray(node, flag);
@@ -270,11 +227,10 @@ bool vtkSlicerTortuosityLogic
     this->GetOrCreateArray<vtkIntArray>(node, "NumberOfPoints");
   assert(nop);
 
-  // 2 - (Re)Fill the metric arrays
-  typedef vtkMRMLSpatialObjectsNode::TubeNetType  TubeNetType;
-  typedef itk::VesselTubeSpatialObject<3>         VesselTubeType;
-  typedef VesselTubeType::TubePointType           VesselTubePointType;
-  typedef itk::SpatialObjectPoint<3>              PointType;
+  // 2 - Fill the metric arrays
+  typedef vtkMRMLSpatialObjectsNode::TubeNetType                    TubeNetType;
+  typedef itk::VesselTubeSpatialObject<3>                           VesselTubeType;
+  typedef itk::tube::TortuositySpatialObjectFilter<VesselTubeType>  FilterType;
 
   TubeNetType* spatialObject = node->GetSpatialObject();
 
@@ -282,10 +238,9 @@ bool vtkSlicerTortuosityLogic
   TubeNetType::ChildrenListType* tubeList =
     spatialObject->GetChildren(spatialObject->GetMaximumDepth(), childName);
 
-  vtkIdType tubeID = 0;
   size_t totalNumberOfPointsAdded = 0;
   for(TubeNetType::ChildrenListType::iterator tubeIt = tubeList->begin();
-        tubeIt != tubeList->end(); ++tubeIt, ++tubeID )
+        tubeIt != tubeList->end(); ++tubeIt)
     {
     VesselTubeType* currTube =
       dynamic_cast<VesselTubeType*>((*tubeIt).GetPointer());
@@ -295,239 +250,38 @@ bool vtkSlicerTortuosityLogic
       }
     assert(currTube->GetNumberOfPoints() >= 2);
 
-    //
-    // DM variables
-    double start[3], end[3];
-    double pathLength = 0.0;
+    // Update filter
+    FilterType::Pointer filter = FilterType::New();
+    filter->SetMeasureFlag(flag);
+    filter->SetInput(currTube);
+    filter->Update();
 
-    // ICM variables
-    double previousN[3] = {0.0, 0.0, 0.0};
-    double T[3], N[3], B[3]; // for the Frenet frame
-    bool vectorBIsValid = false;
-    int inflectionCount = 1;
-
-    InitVector3(T);
-    InitVector3(N);
-    InitVector3(B);
-
-    // SOAM variables
-    double totalCurvature = 0.0;
-
-    size_t numberOfPoints = currTube->GetPoints().size();
-    for(size_t index = 0; index < numberOfPoints; ++index)
+    if (filter->GetOutput()->GetId() != currTube->GetId())
       {
-      VesselTubePointType* point =
-        dynamic_cast<VesselTubePointType*>(currTube->GetPoint(index));
-      assert(point);
-
-      double currentPoint[3];
-      CopyPoints<PointType>(currTube->GetPoint(index), currentPoint);
-
-      //
-      // General variables
-      bool nextPointAvailable = (index < numberOfPoints - 1);
-      double nextPoint[3] = {0.0, 0.0, 0.0};
-      if (nextPointAvailable)
-        {
-        CopyPoints<PointType>(currTube->GetPoint(index + 1), nextPoint);
-        }
-      bool previousPointAvailable = (index > 0);
-      double previousPoint[3] = {0.0, 0.0, 0.0};
-      if (previousPointAvailable)
-        {
-        CopyPoints<PointType>(currTube->GetPoint(index - 1), previousPoint);
-        }
-      // t1 and t2, used both in icm and soam
-      double t1[3] = {0.0, 0.0, 0.0};
-      double t2[3] = {0.0, 0.0, 0.0};
-      if (previousPointAvailable && nextPointAvailable)
-        {
-        vtkMath::Subtract(currentPoint, previousPoint, t1);
-        vtkMath::Subtract(nextPoint, currentPoint, t2);
-        }
-
-      bool nPlus2PointAvailable = (index < numberOfPoints - 2);
-      double nPlus2Point[3] = {0.0, 0.0, 0.0};
-      if (nPlus2PointAvailable)
-        {
-        CopyPoints<PointType>(currTube->GetPoint(index + 2), nPlus2Point);
-        }
-
-      //
-      // DM Computations
-      if (index == 0)
-        {
-        CopyVector3(currentPoint, start);
-        }
-      if (index == numberOfPoints - 1)
-        {
-        CopyVector3(currentPoint, end);
-        }
-
-      if ((dm || icm || ip || soam) && nextPointAvailable)
-        {
-        double currentToNext[3];
-        vtkMath::Subtract(nextPoint, currentPoint, currentToNext);
-        pathLength += vtkMath::Norm(currentToNext);
-        }
-
-      //
-      // ICM Computations
-      double inflectionValue = 0.0;
-      if ((icm || ip) && previousPointAvailable && nextPointAvailable)
-        {
-        // Compute velocity and acceleration
-        double v[3], a[3];
-        vtkMath::Subtract(nextPoint, previousPoint, v);
-        vtkMath::Subtract(t2, t1, a);
-
-        // Compute the Frenet frame
-        // 1 - T = v / |v|
-        CopyVector3(v, T);
-        vtkMath::Normalize(T);
-
-        // 2 - N = v × a × v / |v × a × v|
-        bool canCheckForinflection = vtkMath::Norm(a) > 1e-6;
-        if (canCheckForinflection)
-          {
-          vtkMath::Cross(v, a, N);
-          vtkMath::Cross(v, N, N);
-          vtkMath::Normalize(N);
-          vectorBIsValid = true;
-          }
-        else if (vectorBIsValid) // 2nd chance
-          {
-          // Acceleration can be null when the curve approximates a straight
-          // line (sin around pi for example). Unfortunately that could happen
-          // when the curve is crossing the straight line and the inflection
-          // would be missed...
-          // This assumes that no pure torsion along the N vector happened.
-          // Note that this is only valid is B was already computed at least
-          // once.
-          vtkMath::Cross(B, T, N);
-          vtkMath::Normalize(N);
-          canCheckForinflection = true;
-          }
-
-        // 3 - B = T x N (in case of null acceleration. See above)
-        vtkMath::Cross(T, N, B);
-        vtkMath::Normalize(B);
-
-        if (canCheckForinflection)
-          {
-          // Check for inflection
-          double deltaN[3];
-          vtkMath::Subtract(N, previousN, deltaN);
-
-          inflectionValue = vtkMath::Dot(deltaN, deltaN);
-          if (inflectionValue > 1.0 + 1e-6)
-            {
-            inflectionCount += 1;
-            }
-          }
-
-        CopyVector3(N, previousN);
-        }
-
-      // Set the inflection value for this point
-      if (ip)
-        {
-        ip->SetValue(index, inflectionValue);
-        }
-
-      //
-      // SOAM Computations
-      if (soam &&
-        previousPointAvailable && nextPointAvailable && nPlus2PointAvailable)
-        {
-        double t3[3];
-        vtkMath::Subtract(nPlus2Point, nextPoint, t3);
-
-        // Compute in-plane angle
-        double normT1[3], normT2[3];
-        CopyVector3(t1, normT1);
-        vtkMath::Normalize(normT1);
-        CopyVector3(t2, normT2);
-        vtkMath::Normalize(normT2);
-
-        double inPlaneAngle = SafeAcos( vtkMath::Dot(normT1, normT2) );
-
-        // Compute torsionnal angle
-        double t1t2Cross[3];
-        vtkMath::Cross(t1, t2, t1t2Cross);
-        double t2t3Cross[3];
-        vtkMath::Cross(t2, t3, t2t3Cross);
-
-        double torsionAngle = 0.0;
-        double t1t2Norm = vtkMath::Normalize(t1t2Cross);
-        double t2t3Norm = vtkMath::Normalize(t2t3Cross);
-        if (t1t2Norm > 1e-6 || t2t3Norm > 1e-6)
-          {
-          // We need to make sure we don't artificially scale those vectors
-          // back to life. Otherwise we end up with a torsion angles where
-          // there should not be one
-          torsionAngle = SafeAcos( vtkMath::Dot(t1t2Cross, t2t3Cross) );
-          }
-        // Finally get the curvature
-        totalCurvature +=
-          sqrt(inPlaneAngle*inPlaneAngle + torsionAngle*torsionAngle);
-        }
+      std::cerr<<"Error while running filter on tube."<<std::endl;
+      return false;
       }
 
-    //
-    // DM final calculation
-    double dmResult = -1.0;
-    if (dm || icm)
-      {
-      double startToEnd[3];
-      vtkMath::Subtract(start, end, startToEnd);
-
-      double straighLineLength = vtkMath::Norm(startToEnd);
-      if (straighLineLength > 0.0)
-        {
-        dmResult = pathLength / straighLineLength;
-        }
-
-      if (dmResult < 1.0)
-        {
-        std::cerr<<"Error while computing the distance metric."
-          <<"DM (="<<dmResult<<") > 1.0"<<std::endl;
-        noProblem = false;
-        }
-      }
-
-    // ICM final calculation
-    double icmResult = inflectionCount * dmResult;
-
-    // SOAM final calculation
-    double soamResult = 0.0;
-    if (soam && pathLength > 0.0)
-      {
-      soamResult = totalCurvature / pathLength;
-      }
-    else if (soam && pathLength <= 0.0)
-      {
-      std::cerr<<"Cannot compute SOAM, total tube path (="
-        <<pathLength<<") <= 0.0"<<std::endl;
-      noProblem = false;
-      }
-
-    //
     // Fill the arrays
+    size_t numberOfPoints = currTube->GetPoints().size();
     for(size_t index = totalNumberOfPointsAdded;
       index < numberOfPoints + totalNumberOfPointsAdded; ++index)
       {
       if (dm)
         {
-        dm->SetValue(index, dmResult);
+        dm->SetValue(index, filter->GetDistanceMetric());
         }
       if (icm)
         {
-        icm->SetValue(index, icmResult);
+        icm->SetValue(index, filter->GetInflectionCountMetric());
         }
       if (soam)
         {
-        soam->SetValue(index, soamResult);
+        soam->SetValue(index, filter->GetSumOfAnglesMetric());
+        }
+      if (ip)
+        {
+        ip->SetValue(index, filter->GetInflectionPointValue(index));
         }
       }
     nop->InsertNextValue(numberOfPoints);
@@ -535,7 +289,7 @@ bool vtkSlicerTortuosityLogic
     totalNumberOfPointsAdded += numberOfPoints;
     }
 
-  return noProblem;
+  return true;
 }
 
 //------------------------------------------------------------------------------
