7a8d1dda21e45009f0ecf831d6ec2ed1d96b9573
Lucas Gandel <lucas.gandel@gmail.com>
2015-08-27 18:00:36 -0400

    BUG: Fix bad clipping

--- Applications/ClipTubes/ClipTubes.cxx
+++ Applications/ClipTubes/ClipTubes.cxx
@@ -33,6 +33,11 @@
 
 #include "ClipTubesCLP.h"
 
+#include <vtkCubeSource.h>
+#include <vtkPolyDataWriter.h>
+#include <vtkSmartPointer.h>
+#include <vtkNew.h>
+
 template< unsigned int VDimension >
 bool isInside (itk::Point<double,VDimension> pointPos, double tubeRadius,
                  std::vector<double> boxPos, std::vector<double> boxSize)
@@ -41,7 +46,7 @@ bool isInside (itk::Point<double,VDimension> pointPos, double tubeRadius,
   // is included in the box.
   // A slice is considered as a point and an associated radius
   bool hasXInside=false;
-  bool hasYInside=false,
+  bool hasYInside=false;
   bool hasZInside=false;
   
   if( pointPos[0] + tubeRadius >= boxPos[0] &&
@@ -49,8 +54,8 @@ bool isInside (itk::Point<double,VDimension> pointPos, double tubeRadius,
     {
     hasXInside = true;
     }
-  if( pointPos[1] <= boxPos[1] &&
-    pointPos[1] >= boxPos[1] - boxSize[1] )
+  if( pointPos[1] - tubeRadius <= boxPos[1] &&
+    pointPos[1] + tubeRadius >= boxPos[1] - boxSize[1] )
     {
     hasYInside = true;
     }
@@ -63,7 +68,7 @@ bool isInside (itk::Point<double,VDimension> pointPos, double tubeRadius,
       }
     case 3:
       {
-      if( pointPos[2] + tubeRadius >= boxPos[2] &&
+      if( pointPos[2] + tubeRadius  >= boxPos[2] &&
         pointPos[2] - tubeRadius <= boxPos[2] + boxSize[2] )
         {
         hasZInside = true;
@@ -81,6 +86,25 @@ bool isInside (itk::Point<double,VDimension> pointPos, double tubeRadius,
   return (hasXInside && hasYInside && hasZInside);
 }
 
+void writeBox(std::vector<double> boxPos, std::vector<double> boxSize,
+  double spacing)
+{
+  // This function return a .vtk file that can be loaded
+  // in VesselView to draw the box. Uncomment function
+  // call (l. 286) to use it and change the hardcoded path
+  // of the file below
+  vtkSmartPointer<vtkCubeSource> cubeSource =
+    vtkSmartPointer<vtkCubeSource>::New();
+  cubeSource->SetBounds(
+    -spacing * (boxPos[0] + boxSize[0]), -spacing * boxPos[0],
+    -spacing * boxPos[1], -spacing * (boxPos[1] - boxSize[1]),
+    spacing * boxPos[2], spacing * (boxPos[2] + boxSize[2]) );
+  vtkNew<vtkPolyDataWriter> writer;
+  writer->SetFileName(
+    "/home/lucas/Projects/ClipTubes/ClipTubes-data/BOX.vtk" );
+  writer->SetInputConnection( cubeSource->GetOutputPort() );
+  writer->Write();
+}
 
 template< unsigned int VDimension >
 int DoIt (int argc, char * argv[])
@@ -145,8 +169,21 @@ int DoIt (int argc, char * argv[])
   //Target Group to save desired tubes
   typename TubeGroupType::Pointer pTargetTubeGroup = TubeGroupType::New();
   
-  int targetTubeId=0;
+  pTargetTubeGroup->CopyInformation( pSourceTubeGroup );
+
+  // TODO: make CopyInformation of itk::SpatialObject do this
+  pTargetTubeGroup->GetObjectToParentTransform()->SetScale(
+    pSourceTubeGroup->GetObjectToParentTransform()->GetScale() );
+  pTargetTubeGroup->GetObjectToParentTransform()->SetOffset(
+    pSourceTubeGroup->GetObjectToParentTransform()->GetOffset() );
+  pTargetTubeGroup->GetObjectToParentTransform()->SetMatrix(
+    pSourceTubeGroup->GetObjectToParentTransform()->GetMatrix() );
+  pTargetTubeGroup->SetSpacing( pSourceTubeGroup->GetSpacing() );
+  pTargetTubeGroup->ComputeObjectToWorldTransform();
   
+  int targetTubeId=0;
+  double spacing=0.0;//spacing factor to draw the box
+
   for( typename TubeGroupType::ChildrenListType::iterator
     tubeList_it = pSourceTubeList->begin();
     tubeList_it != pSourceTubeList->end(); ++tubeList_it)
@@ -193,6 +230,21 @@ int DoIt (int argc, char * argv[])
           {
           //**** Target Tube **** :
           typename TubeType::Pointer pTargetTube = TubeType::New();
+
+          pTargetTube->CopyInformation( pCurSourceTube );
+
+          // TODO: make CopyInformation of itk::SpatialObject do this
+          pTargetTube->GetObjectToParentTransform()->SetScale(
+            pCurSourceTube->GetObjectToParentTransform()->GetScale() );
+          pTargetTube->GetObjectToParentTransform()->SetOffset(
+            pCurSourceTube->GetObjectToParentTransform()->GetOffset() );
+          pTargetTube->GetObjectToParentTransform()->SetMatrix(
+            pCurSourceTube->GetObjectToParentTransform()->GetMatrix() );
+          pTargetTube->SetSpacing( pCurSourceTube->GetSpacing() );
+          pTargetTube->ComputeObjectToWorldTransform();
+
+          pTargetTube->ComputeTangentAndNormals();
+
           pTargetTube->SetId(targetTubeId);
           ++targetTubeId;
           //Save clipped tube
@@ -203,7 +255,38 @@ int DoIt (int argc, char * argv[])
           }
         }
       }
+    if( TargetPointList.size() > 0 )
+      {
+      //**** Target Tube **** :
+      typename TubeType::Pointer pTargetTube = TubeType::New();
+
+      pTargetTube->CopyInformation( pCurSourceTube );
+
+      // TODO: make CopyInformation of itk::SpatialObject do this
+      pTargetTube->GetObjectToParentTransform()->SetScale(
+        pCurSourceTube->GetObjectToParentTransform()->GetScale() );
+      pTargetTube->GetObjectToParentTransform()->SetOffset(
+        pCurSourceTube->GetObjectToParentTransform()->GetOffset() );
+      pTargetTube->GetObjectToParentTransform()->SetMatrix(
+        pCurSourceTube->GetObjectToParentTransform()->GetMatrix() );
+      pTargetTube->SetSpacing( pCurSourceTube->GetSpacing() );
+      pTargetTube->ComputeObjectToWorldTransform();
+
+      pTargetTube->ComputeTangentAndNormals();
+
+      pTargetTube->SetId(targetTubeId);
+      ++targetTubeId;
+      //Save clipped tube
+      pTargetTube->SetPoints(TargetPointList);
+      pTargetTubeGroup->AddSpatialObject(pTargetTube);
+
+      TargetPointList.clear();
+      }
+    spacing=*(pCurSourceTube->GetSpacing());
     }
+  //writeBox(boxCorner, boxSize, spacing); //Return a .vtk file of the box
+
+
   timeCollector.Stop( "Selecting Tubes" );
   
   // Write output TRE file
