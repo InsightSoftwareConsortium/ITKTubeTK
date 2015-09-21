930f165dc8c45884b5b298e0e1ff1838c72ab43c
Stephen R. Aylward <stephen.aylward@kitware.com>
2015-09-21 08:56:48 -0400

    BUG and STYLE: Many style issues. Spacing not fully used. Needs tests.

--- Applications/ClipTubes/ClipTubes.cxx
+++ Applications/ClipTubes/ClipTubes.cxx
@@ -1,17 +1,17 @@
 /*=========================================================================
    Library:   TubeTK
-   
+
    Copyright 2010 Kitware Inc. 28 Corporate Drive,
    Clifton Park, NY, 12065, USA.
-   
+
    All rights reserved.
-   
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-   
+
     http://www.apache.org/licenses/LICENSE-2.0
-    
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,17 +38,17 @@
 #include <vtkSmartPointer.h>
 #include <vtkNew.h>
 
-template< unsigned int VDimension >
-bool isInside (itk::Point<double,VDimension> pointPos, double tubeRadius,
-                 std::vector<double> boxPos, std::vector<double> boxSize)
+template< unsigned int DimensionT >
+bool isInside( itk::Point< double, DimensionT > pointPos, double tubeRadius,
+  std::vector< double > boxPos, std::vector< double > boxSize )
 {
   // Return a boolean indicating if any slice of the tube
   // is included in the box.
   // A slice is considered as a point and an associated radius
-  bool hasXInside=false;
-  bool hasYInside=false;
-  bool hasZInside=false;
-  
+  bool hasXInside = false;
+  bool hasYInside = false;
+  bool hasZInside = false;
+
   if( pointPos[0] + tubeRadius >= boxPos[0] &&
     pointPos[0] - tubeRadius <= boxPos[0] + boxSize[0] )
     {
@@ -59,7 +59,7 @@ bool isInside (itk::Point<double,VDimension> pointPos, double tubeRadius,
     {
     hasYInside = true;
     }
-  switch( VDimension )
+  switch( DimensionT )
     {
     case 2:
       {
@@ -82,31 +82,43 @@ bool isInside (itk::Point<double,VDimension> pointPos, double tubeRadius,
       return EXIT_FAILURE;
       }
     }
- 
+
   return (hasXInside && hasYInside && hasZInside);
 }
 
-void writeBox(std::vector<double> boxPos, std::vector<double> boxSize,
-  double spacing)
+template< unsigned int DimensionT >
+void writeBox( std::vector< double > boxPos, std::vector< double > boxSize,
+  std::vector< double > spacing, std::string boxFileName )
 {
-  // This function return a .vtk file that can be loaded
-  // in VesselView to draw the box. Uncomment function
-  // call (l. 286) to use it and change the hardcoded path
-  // of the file below
-  vtkSmartPointer<vtkCubeSource> cubeSource =
-    vtkSmartPointer<vtkCubeSource>::New();
-  cubeSource->SetBounds(
-    -spacing * (boxPos[0] + boxSize[0]), -spacing * boxPos[0],
-    -spacing * boxPos[1], -spacing * (boxPos[1] - boxSize[1]),
-    spacing * boxPos[2], spacing * (boxPos[2] + boxSize[2]) );
-  vtkNew<vtkPolyDataWriter> writer;
-  writer->SetFileName(
-    "/home/lucas/Projects/ClipTubes/ClipTubes-data/BOX.vtk" );
-  writer->SetInputConnection( cubeSource->GetOutputPort() );
-  writer->Write();
+  if( DimensionT == 3 )
+    {
+    vtkSmartPointer<vtkCubeSource> cubeSource =
+      vtkSmartPointer<vtkCubeSource>::New();
+    cubeSource->SetBounds(
+      -spacing[0] * (boxPos[0] + boxSize[0]), -spacing[0] * boxPos[0],
+      -spacing[1] * boxPos[1], -spacing[1] * (boxPos[1] - boxSize[1]),
+      spacing[2] * boxPos[2], spacing[2] * (boxPos[2] + boxSize[2]) );
+    vtkNew<vtkPolyDataWriter> writer;
+    writer->SetFileName( boxFileName.c_str() );
+    writer->SetInputConnection( cubeSource->GetOutputPort() );
+    writer->Write();
+    }
+  else
+    {
+    vtkSmartPointer< vtkCubeSource > cubeSource =
+      vtkSmartPointer< vtkCubeSource >::New();
+    cubeSource->SetBounds(
+      -spacing[0] * (boxPos[0] + boxSize[0]), -spacing[0] * boxPos[0],
+      -spacing[1] * boxPos[1], -spacing[1] * (boxPos[1] - boxSize[1]),
+      0, 0);
+    vtkNew<vtkPolyDataWriter> writer;
+    writer->SetFileName( boxFileName.c_str() );
+    writer->SetInputConnection( cubeSource->GetOutputPort() );
+    writer->Write();
+    }
 }
 
-template< unsigned int VDimension >
+template< unsigned int DimensionT >
 int DoIt (int argc, char * argv[])
 {
   PARSE_ARGS;
@@ -114,34 +126,28 @@ int DoIt (int argc, char * argv[])
   // Ensure that the input image dimension is valid
   // We only support 2D and 3D Images due to the
   // limitation of itkTubeSpatialObject
-  if( VDimension != 2 && VDimension != 3 )
+  if( DimensionT != 2 && DimensionT != 3 )
     {
     tube::ErrorMessage(
       "Error: Only 2D and 3D data is currently supported.");
     return EXIT_FAILURE;
     }
-    
+
   // The timeCollector to perform basic profiling of algorithmic components
   itk::TimeProbesCollectorBase timeCollector;
-  
+
   // Load TRE File
   tubeStandardOutputMacro( << "\n>> Loading TRE File" );
 
-  typedef itk::SpatialObjectReader< VDimension >          TubesReaderType;
-  typedef itk::GroupSpatialObject< VDimension >           TubeGroupType;
-//typedef itk::TubeSpatialObject< VDimension >            TubeType;
-/*WARNING :
-  dynamic_cast on "typedef itk::TubeSpatialObject< VDimension > TubeType"
-  causes SEGFAULT
-  so TubeType corresponds to VesselTubeType to prevent issues
-  so TubePointType corresponds to VesselTubePointType */
-  typedef itk::VesselTubeSpatialObject< VDimension >      TubeType;
-  typedef itk::VesselTubeSpatialObjectPoint< VDimension > TubePointType;
+  typedef itk::SpatialObjectReader< DimensionT >          TubesReaderType;
+  typedef itk::GroupSpatialObject< DimensionT >           TubeGroupType;
+  typedef itk::VesselTubeSpatialObject< DimensionT >      TubeType;
+  typedef itk::VesselTubeSpatialObjectPoint< DimensionT > TubePointType;
 
   timeCollector.Start( "Loading Input TRE File" );
-  
+
   typename TubesReaderType::Pointer tubeFileReader = TubesReaderType::New();
-  
+
   try
     {
     tubeFileReader->SetFileName( inputTREFile.c_str() );
@@ -154,21 +160,21 @@ int DoIt (int argc, char * argv[])
     timeCollector.Report();
     return EXIT_FAILURE;
     }
-  
+
   typename TubeGroupType::Pointer pSourceTubeGroup =
     tubeFileReader->GetGroup();
   typename TubeGroupType::ChildrenListPointer pSourceTubeList =
     pSourceTubeGroup->GetChildren();
-  
+
   timeCollector.Stop( "Loading Input TRE File" );
-  
+
   // Compute clipping
   tubeStandardOutputMacro( << "\n>> Finding Tubes for Clipping" );
-  
+
   timeCollector.Start( "Selecting Tubes" );
   //Target Group to save desired tubes
   typename TubeGroupType::Pointer pTargetTubeGroup = TubeGroupType::New();
-  
+
   pTargetTubeGroup->CopyInformation( pSourceTubeGroup );
 
   // TODO: make CopyInformation of itk::SpatialObject do this
@@ -180,9 +186,9 @@ int DoIt (int argc, char * argv[])
     pSourceTubeGroup->GetObjectToParentTransform()->GetMatrix() );
   pTargetTubeGroup->SetSpacing( pSourceTubeGroup->GetSpacing() );
   pTargetTubeGroup->ComputeObjectToWorldTransform();
-  
+
   int targetTubeId=0;
-  double spacing=0.0;//spacing factor to draw the box
+  std::vector< double > spacing( DimensionT, 0 );
 
   for( typename TubeGroupType::ChildrenListType::iterator
     tubeList_it = pSourceTubeList->begin();
@@ -201,7 +207,7 @@ int DoIt (int argc, char * argv[])
     //Get points in current source tube
     typename TubeType::PointListType pointList =
       pCurSourceTube->GetPoints();
-      
+
     for( typename TubeType::PointListType::const_iterator
       pointList_it = pointList.begin();
       pointList_it != pointList.end(); ++pointList_it )
@@ -210,17 +216,18 @@ int DoIt (int argc, char * argv[])
       typename TubePointType::PointType curSourcePos =
         curSourcePoint.GetPosition();
       //Save point in target tube if it belongs to the box
-      if(isInside(curSourcePos,curSourcePoint.GetRadius(),boxCorner,boxSize))
+      if( isInside( curSourcePos, curSourcePoint.GetRadius(),
+        boxCorner, boxSize ) )
         {
-        if(ClipTubes)
+        if( ClipTubes )
           {
-          TargetPointList.push_back(curSourcePoint);
+          TargetPointList.push_back( curSourcePoint );
           }
         else
           {
-          pCurSourceTube->SetId(targetTubeId);
+          pCurSourceTube->SetId( targetTubeId );
           ++targetTubeId;
-          pTargetTubeGroup->AddSpatialObject(pCurSourceTube);
+          pTargetTubeGroup->AddSpatialObject( pCurSourceTube );
           break;
           }
         }
@@ -245,11 +252,11 @@ int DoIt (int argc, char * argv[])
 
           pTargetTube->ComputeTangentAndNormals();
 
-          pTargetTube->SetId(targetTubeId);
+          pTargetTube->SetId( targetTubeId );
           ++targetTubeId;
           //Save clipped tube
-          pTargetTube->SetPoints(TargetPointList);
-          pTargetTubeGroup->AddSpatialObject(pTargetTube);
+          pTargetTube->SetPoints( TargetPointList );
+          pTargetTubeGroup->AddSpatialObject( pTargetTube );
 
           TargetPointList.clear();
           }
@@ -274,28 +281,34 @@ int DoIt (int argc, char * argv[])
 
       pTargetTube->ComputeTangentAndNormals();
 
-      pTargetTube->SetId(targetTubeId);
+      pTargetTube->SetId( targetTubeId );
       ++targetTubeId;
       //Save clipped tube
-      pTargetTube->SetPoints(TargetPointList);
-      pTargetTubeGroup->AddSpatialObject(pTargetTube);
+      pTargetTube->SetPoints( TargetPointList );
+      pTargetTubeGroup->AddSpatialObject( pTargetTube );
 
       TargetPointList.clear();
       }
-    spacing=*(pCurSourceTube->GetSpacing());
+    for( unsigned int d=0; d<DimensionT; ++d )
+      {
+      spacing[d] = pCurSourceTube->GetSpacing()[d];
+      }
     }
-  //writeBox(boxCorner, boxSize, spacing); //Return a .vtk file of the box
+  if( !outputBoxFile.empty() )
+    {
+    writeBox<DimensionT>( boxCorner, boxSize, spacing, outputBoxFile );
+    }
 
 
   timeCollector.Stop( "Selecting Tubes" );
-  
+
   // Write output TRE file
   tubeStandardOutputMacro(
     << "\n>> Writing TRE file" );
 
   timeCollector.Start( "Writing output TRE file" );
-  
-  typedef itk::SpatialObjectWriter< VDimension > TubeWriterType;
+
+  typedef itk::SpatialObjectWriter< DimensionT > TubeWriterType;
   typename TubeWriterType::Pointer tubeWriter = TubeWriterType::New();
 
   try
@@ -331,17 +344,17 @@ int main( int argc, char * argv[] )
     return EXIT_FAILURE;
     }
   PARSE_ARGS;
-  
-  if(boxCorner.empty() || boxSize.empty())
+
+  if( boxCorner.empty() || boxSize.empty() )
     {
     tube::ErrorMessage(
       "Error: longflags --boxCorner and --boxSize are both required");
     return EXIT_FAILURE;
     }
-  
+
   MetaScene *mScene = new MetaScene;
   mScene->Read( inputTREFile.c_str() );
-  
+
   if( mScene->GetObjectList()->empty() )
     {
     tubeWarningMacro( << "Input TRE file has no spatial objects" );
