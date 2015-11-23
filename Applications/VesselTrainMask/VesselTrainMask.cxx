ba87f9d1b9d542ee4f6314269639cdadeb19e2c7
Sumedha Singla <sumedha.singla@kitware.com>
2015-11-23 16:46:00 -0500

    ENH: Added functionality to the new CLI.

--- Applications/VesselTrainMask/VesselTrainMask.cxx
+++ Applications/VesselTrainMask/VesselTrainMask.cxx
@@ -22,48 +22,255 @@
 #include <sstream>
 
 #include "itkTimeProbesCollectorBase.h"
+#include "tubeCLIProgressReporter.h"
 #include "tubeMessage.h"
 
 #include "tubeMacro.h"
-#include "metaScene.h"
-
-#include "itkSpatialObjectReader.h"
-#include "itkSpatialObjectWriter.h"
-#include "itkGroupSpatialObject.h"
 
 #include <itkImageFileReader.h>
 #include <itkImageFileWriter.h>
 
+#include <itkBinaryThinningImageFilter.h>
+#include <itkBinaryThresholdImageFilter.h>
+#include <itkBinaryBallStructuringElement.h>
+#include <itkDilateObjectMorphologyImageFilter.h>
+#include <itkErodeObjectMorphologyImageFilter.h>
+#include <itkAddImageFilter.h>
+#include <itkCastImageFilter.h>
+
 #include "VesselTrainMaskCLP.h"
 
-#include <vtkCubeSource.h>
 #include <vtkPolyDataWriter.h>
 #include <vtkSmartPointer.h>
 #include <vtkNew.h>
 
-// Description:
-// Get the ComponentType and dimension of the image
-void GetImageInformation(std::string fileName,
-  itk::ImageIOBase::IOComponentType &componentType,
-  unsigned int & dimension)
+template< class TPixel, unsigned int VDimension >
+int DoIt( int argc, char * argv[] );
+
+// Must follow include of "...CLP.h" and forward declaration of int DoIt( ... ).
+#include "tubeCLIHelperFunctions.h"
+
+template< class TPixel, unsigned int VDimension >
+typename itk::Image< TPixel, VDimension >::Pointer
+findCenterLines( typename itk::Image< TPixel, VDimension >::Pointer input )
 {
-  itk::ImageIOBase::Pointer imageIO =
-    itk::ImageIOFactory::CreateImageIO(fileName.c_str(),
-    itk::ImageIOFactory::ReadMode);
-  if (!imageIO)
+  typedef itk::Image< TPixel, VDimension >                        ImageType;
+  typedef itk::BinaryThinningImageFilter< ImageType, ImageType >  FilterType;
+
+  typename ImageType::Pointer output = input;
+
+  typename FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( input );
+  filter->Update();
+  output = filter->GetOutput();
+  return output;
+}
+
+template< class TPixel, unsigned int VDimension >
+void
+thresholdVolume( typename itk::Image< TPixel, VDimension >::Pointer &input,
+float threshLow,
+float threshHigh,
+float valTrue,
+float valFalse )
+{
+  typedef itk::Image< TPixel, VDimension >                        ImageType;
+  typedef itk::BinaryThresholdImageFilter< ImageType, ImageType > ThresholdType;
+
+  typename ImageType::Pointer output = input;
+
+  typename ThresholdType::Pointer threshold = ThresholdType::New();
+  threshold->SetInput( input );
+  threshold->SetLowerThreshold( threshLow);
+  threshold->SetUpperThreshold( threshHigh );
+  threshold->SetInsideValue( valTrue );
+  threshold->SetOutsideValue( valFalse );
+  threshold->Update();
+  input = threshold->GetOutput();
+  return;
+}
+
+template< class TPixel, unsigned int VDimension >
+void
+morphologyVolume( typename itk::Image< TPixel, VDimension >::Pointer &input, int mode,
+float radius,
+float foregroundValue,
+float backgroundValue )
+{
+  typedef itk::Image< TPixel, VDimension >                          ImageType;
+  typedef itk::BinaryBallStructuringElement< TPixel, VDimension >   BallType;
+  BallType ball;
+  ball.SetRadius( 1 );
+  ball.CreateStructuringElement();
+
+  typename ImageType::Pointer output = input;
+
+  typedef itk::ErodeObjectMorphologyImageFilter
+    < ImageType, ImageType, BallType >       ErodeFilterType;
+  typedef itk::DilateObjectMorphologyImageFilter
+    < ImageType, ImageType, BallType >       DilateFilterType;
+
+  switch ( mode )
     {
-    std::cerr << "NO IMAGEIO WAS FOUND" << std::endl;
-    dimension = 0;
-    return;
+    case 0:
+      {
+      for ( int r = 0; r<radius; r++ )
+    {
+      typename ErodeFilterType::Pointer filter =
+        ErodeFilterType::New();
+      filter->SetBackgroundValue( backgroundValue );
+      filter->SetKernel( ball );
+      filter->SetObjectValue( foregroundValue );
+      filter->SetInput( input );
+      filter->Update();
+      input = filter->GetOutput();
+    }
+    break;
+      }
+    case 1:
+      {
+      for ( int r = 0; r<radius; r++ )
+    {
+      typename DilateFilterType::Pointer filter =
+        DilateFilterType::New();
+      filter->SetKernel( ball );
+      filter->SetObjectValue( foregroundValue );
+      filter->SetInput( input );
+      filter->Update();
+      input = filter->GetOutput();
+    }
+    break;
+      }
+    }
+  return;
+}
+
+template< class TPixel, unsigned int VDimension >
+void
+addVolume( typename itk::Image< TPixel, VDimension >::Pointer &input1,
+typename itk::Image< TPixel, VDimension >::Pointer input2,
+float weight1,
+float weight2 )
+{
+  typedef itk::Image< TPixel, VDimension >   ImageType;
+
+  itk::ImageRegionIterator< ImageType > it1(input1,
+    input1->GetLargestPossibleRegion());
+  itk::ImageRegionIterator< ImageType > it2(input2,
+    input2->GetLargestPossibleRegion());
+  it1.GoToBegin();
+  it2.GoToBegin();
+  while ( !it1.IsAtEnd() )
+    {
+    double tf1 = it1.Get();
+    double tf2 = it2.Get();
+    double tf = weight1*tf1 + weight2*tf2;
+    it1.Set( ( TPixel )tf );
+    ++it1;
+    ++it2;
+    }
+  return;
+}
+
+template< class TPixel, unsigned int VDimension >
+void
+saveVolumeAsShort(typename itk::Image< TPixel, VDimension >::Pointer input,
+const char* fileName)
+{
+  typedef itk::Image< short, VDimension >                      ImageTypeShort;
+  typedef itk::Image< TPixel, VDimension >                     ImageType;
+  typedef itk::CastImageFilter< ImageType, ImageTypeShort >    CastFilterType;
+  typename CastFilterType::Pointer castFilter =
+    CastFilterType::New();
+
+  castFilter->SetInput( input );
+  typedef itk::ImageFileWriter< ImageTypeShort >
+    VolumeWriterType;
+  typename VolumeWriterType::Pointer writer =
+    VolumeWriterType::New();
+  writer->SetFileName( fileName );
+  writer->SetInput( castFilter->GetOutput() );
+  writer->SetUseCompression( true );
+  writer->Update();
+  writer->Write();
+}
+
+//Do it
+
+template< class TPixel, unsigned int VDimension >
+int DoIt( int argc, char * argv[] )
+{
+  PARSE_ARGS;
+
+  if ( VDimension != 2 && VDimension != 3 )
+  {
+    tube::ErrorMessage(
+      "Error: Only 2D and 3D data is currently supported.");
+    return EXIT_FAILURE;
   }
 
-  // Now that we found the appropriate ImageIO class, ask it to
-  // read the meta data from the image file.
-  imageIO->SetFileName(fileName.c_str());
-  imageIO->ReadImageInformation();
+  typedef itk::Image< TPixel, VDimension >            ImageType;
+  typedef itk::ImageFileReader< ImageType >           ImageReaderType;
 
-  componentType = imageIO->GetComponentType();
-  dimension = imageIO->GetNumberOfDimensions();
+  // The timeCollector to perform basic profiling of algorithmic components
+  itk::TimeProbesCollectorBase timeCollector;
+
+  timeCollector.Start( "Loading Input Volume Mask File" );
+  // CLIProgressReporter is used to communicate progress with the Slicer GUI
+  tube::CLIProgressReporter progressReporter( "VesselTrainMask",
+    CLPProcessInformation );
+  progressReporter.Start();
+  float progress = 0;
+
+  ImageReaderType::Pointer imReader = ImageReaderType::New();
+  typename ImageType::Pointer image;
+  tube::InfoMessage( "Reading volume mask..." );
+  imReader->SetFileName(inputVolume.c_str());
+  try
+    {
+    imReader->Update();
+    image = imReader->GetOutput();
+    }
+  catch ( itk::ExceptionObject & err )
+    {
+    tube::FmtErrorMessage( "Cannot read volume mask file: %s",
+      err.what() );
+    return EXIT_FAILURE;
+  }
+  timeCollector.Stop( "Loading Input Volume Mask File" );
+  progress = 0.35;
+  progressReporter.Report( progress );
+
+  timeCollector.Start( "Find Center Lines" );
+  tube::InfoMessage( "Finding Center Lines..." );
+  typename ImageType::Pointer centerLines;
+  centerLines = findCenterLines< TPixel, VDimension >( image );
+  timeCollector.Stop( "Find Center Lines" );
+  progress = 0.65;
+  progressReporter.Report( progress );
+  timeCollector.Start( "Threshold and Mathematical Morphology" );
+  tube::InfoMessage( "Thresholding..." );
+  thresholdVolume< TPixel, VDimension >( image, 0, gap, 0, 255 );
+  morphologyVolume< TPixel, VDimension >( image, 1, notVesselWidth, 255, 0 );
+  typename ImageType::Pointer dialatedImage;
+  dialatedImage = image;
+  morphologyVolume< TPixel, VDimension >( image, 1, notVesselWidth, 255, 0 );
+  tube::InfoMessage( "Creating Not-Vessel Mask..." );
+  addVolume< TPixel, VDimension >( image, dialatedImage, 1, -1 );
+  if ( !notVesselMask.empty() )
+    {
+    saveVolumeAsShort< TPixel, VDimension >( image, notVesselMask.c_str() );
+    }
+  tube::InfoMessage( "Creating Vessel Mask..." );
+  addVolume< TPixel, VDimension >( image, centerLines, 0.5, 255 );
+  saveVolumeAsShort< TPixel, VDimension >( image, outputVolume.c_str() );
+  timeCollector.Stop( "Threshold and Mathematical Morphology" );
+  progress = 1.0;
+  progressReporter.Report( progress );
+  progressReporter.End();
+
+  timeCollector.Report();
+  return EXIT_SUCCESS;
 }
 
 // Main
@@ -73,11 +280,14 @@ int main( int argc, char * argv[] )
     {
     PARSE_ARGS;
     }
-  catch( const std::exception & err )
+  catch ( const std::exception & err )
     {
     tube::ErrorMessage( err.what() );
     return EXIT_FAILURE;
     }
   PARSE_ARGS;
 
+  // You may need to update this line if, in the project's .xml CLI file,
+  //   you change the variable name for the inputVolume.
+  return tube::ParseArgsAndCallDoIt( inputVolume, argc, argv );
 }
