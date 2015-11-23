a062857c003b032d7aa24d23f80f7ffaa9a9e99e
Stephen R. Aylward <stephen.aylward@kitware.com>
2015-11-22 22:51:38 -0500

    ENH: Bump versions of ExternalProjects

--- CMake/Superbuild/ExternalProjectsConfig.cmake
+++ CMake/Superbuild/ExternalProjectsConfig.cmake
@@ -25,25 +25,26 @@
 
 
 # Cppcheck version 1.61
-set( Cppcheck_URL
-  ${github_protocol}://github.com/danmar/cppcheck.git )
+set( Cppcheck_URL ${github_protocol}://github.com/danmar/cppcheck.git )
 set( Cppcheck_HASH_OR_TAG 1.61 )
 
 # TubeTK Image Viewer
-set( ImageViewer_URL ${github_protocol}://github.com/KitwareMedical/ImageViewer.git )
+set( ImageViewer_URL
+  ${github_protocol}://github.com/KitwareMedical/ImageViewer.git )
 set( ImageViewer_HASH_OR_TAG 2f728d05fe159828e6221e1420b9c3a00295315b )
 
 # JsonCpp
 # ${svn_protocol}://svn.code.sf.net/p/jsoncpp/code/trunk/jsoncpp )
-set( JsonCpp_URL http://midas3.kitware.com/midas/download/bitstream/366544/JsonCpp_r276.tar.gz )
-set( JsonCpp_HASH_OR_TAG 192f0cf2b00798d4f4fb29c99a3aa83c )
+# http://midas3.kitware.com/midas/download/bitstream/366544/JsonCpp_r276.tar.gz
+set( JsonCpp_URL ${git_protocol}://github.com/open-source-parsers/jsoncpp.git )
+set( JsonCpp_HASH_OR_TAG 9234cbbc90d1f6c70dd5a90b4d533779e45c820c )
 
 # KWStyle
 set( KWStyle_URL
   ${git_protocol}://github.com/Kitware/KWStyle.git )
-set( KWStyle_HASH_OR_TAG d23ea3680dcdd375d6fdab50de9dae7090f73ed6 )
+set( KWStyle_HASH_OR_TAG 22f2e0bd4ec4c94ca85af0ce24747e67a17b4c52 )
 
-# LIBSVM version 3.17 (minimum version 3.1)
+# LIBSVM version 3.2 (minimum version 3.1)
 set( LIBSVM_URL
   ${git_protocol}://github.com/KitwareMedical/libsvm.git )
 set( LIBSVM_HASH_OR_TAG 55fbd26a0bb53efffe89eb65b3a046c3ecc9965f )
@@ -51,23 +52,23 @@ set( LIBSVM_HASH_OR_TAG 55fbd26a0bb53efffe89eb65b3a046c3ecc9965f )
 
 ###########################################################
 ###########################################################
-# The following were copied from Slicer on 2015.08.29
+# The following were copied from Slicer on 11/22/2015
 ###########################################################
 ###########################################################
 
 # Common Toolkit
 set( CTK_URL ${github_protocol}://github.com/commontk/CTK.git )
-set( CTK_HASH_OR_TAG 35a187ff5ae15f1c590a2ac317cd102c5746a81b )
+set( CTK_HASH_OR_TAG 5f581323b6526caf5ff293d9a810a3c9f68343d9 )
 
 # Insight Segmentation and Registration Toolkit
 set( ITK_URL ${github_protocol}://github.com/Slicer/ITK.git )
-set( ITK_HASH_OR_TAG 0905a43149320d8c2993e83ecf6c8a9d6b2b4232 )
+set( ITK_HASH_OR_TAG 03055046e2693349937339975df4e16b598609c1 )
 
 # Slicer Execution Model
 set( SlicerExecutionModel_URL
   ${github_protocol}://github.com/Slicer/SlicerExecutionModel.git )
-set( SlicerExecutionModel_HASH_OR_TAG 608c7b06f9402b35f0ec01550a3b9e313582f683 )
+set( SlicerExecutionModel_HASH_OR_TAG 311eff9038be165c1341dab90196fbd7466e715a )
 
 # Visualization Toolkit (3D Slicer fork)
 set( VTK_URL ${github_protocol}://github.com/Slicer/VTK.git )
-set( VTK_HASH_OR_TAG 716b0f56fc4bd4b9fcca05f386ec375bcd674122 )
+set( VTK_HASH_OR_TAG fe92273888219edca422f3a308761ddcd2882e2b)
