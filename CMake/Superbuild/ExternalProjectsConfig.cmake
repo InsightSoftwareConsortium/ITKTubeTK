4f89a2435043b6370fb177224fbc9c57d5364d87
Stephen R. Aylward <stephen.aylward@kitware.com>
2015-11-28 09:47:13 -0500

    ENH: Bump JsonCpp and remove unused arg from jsconcpp superbuild

--- CMake/Superbuild/ExternalProjectsConfig.cmake
+++ CMake/Superbuild/ExternalProjectsConfig.cmake
@@ -37,7 +37,7 @@ set( ImageViewer_HASH_OR_TAG 2f728d05fe159828e6221e1420b9c3a00295315b )
 # ${svn_protocol}://svn.code.sf.net/p/jsoncpp/code/trunk/jsoncpp )
 # http://midas3.kitware.com/midas/download/bitstream/366544/JsonCpp_r276.tar.gz
 set( JsonCpp_URL ${git_protocol}://github.com/KitwareMedical/jsoncpp.git )
-set( JsonCpp_HASH_OR_TAG 7c335c6270d5f81a2c0eabe85466aaef9282898f )
+set( JsonCpp_HASH_OR_TAG 110d054227e9eb63faad48a1fb6a828ad0670e61 )
 
 # KWStyle
 set( KWStyle_URL
