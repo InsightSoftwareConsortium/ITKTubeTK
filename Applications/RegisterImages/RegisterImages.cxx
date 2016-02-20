536a1cad1671f13b0f57fcdd18f98c0b5d0f3094
Francois Budin <francois.budin@kitware.com>
2016-02-19 19:53:34 -0500

    Revert "ENH: Activation of 2D images support from CMake"
    This reverts commit 09c0adc2dadbfe826f2e589968a29e40a5622206.
    
    * Remove renaming of executable files
    * Remove second optional volume that is passed to tube::ParseArgAndCallDoIt(). RegisterImages uses fixedImage parameters to set the output image parameters
    * Remove command line option of RegisterImage that prints if software was compiled with support for 2D images. The user will see an error message when trying to run the tool on a 2D image if 2D images are not supported.

--- Applications/RegisterImages/RegisterImages.cxx
+++ Applications/RegisterImages/RegisterImages.cxx
@@ -640,7 +640,7 @@ int main( int argc, char * argv[] )
     itk::MultiThreader::SetGlobalDefaultNumberOfThreads( numberOfThreads );
     }
 
-  tube::ParseArgsAndCallDoIt( fixedImage , argc, argv, movingImage );
+  tube::ParseArgsAndCallDoIt( fixedImage, argc, argv );
 
   return EXIT_SUCCESS;
 }
