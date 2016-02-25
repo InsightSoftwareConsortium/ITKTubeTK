0acb1ffc95477ca597b17d4fe3a5addf596f5015
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-02-25 14:10:26 -0500

    STYLE: Rename (Image|Json|Text)CompareCommand to (Image|Json|Text)Compare
    This commit renames the tools to facilitate discovery and be consistent
    with naming convention adopted in the remaining of the toolkit.
    
    It has been done in three steps:
    
    (1) Rename files using this one liner:
    
    $ for file in $(find . | ack "(Json|Image|Text)CompareCommand"); do newfile=$(echo $file | sed -e 's/CompareCommand/Compare/'); echo "Renaming $file to $newfile"; mv $file $newfile; done
    Renaming ./Base/CompareTools/JsonCompareCommand.cxx to ./Base/CompareTools/JsonCompare.cxx
    Renaming ./Base/CompareTools/TextCompareCommand.cxx to ./Base/CompareTools/TextCompare.cxx
    Renaming ./Base/CompareTools/ImageCompareCommand.cxx to ./Base/CompareTools/ImageCompare.cxx
    Renaming ./Base/CompareTools/ImageCompareCommand.xml to ./Base/CompareTools/ImageCompare.xml
    Renaming ./Base/Common/Testing/JsonCompareCommandTestTest2.json to ./Base/Common/Testing/JsonCompareTestTest2.json
    Renaming ./Base/Common/Testing/JsonCompareCommandTestTest1.json to ./Base/Common/Testing/JsonCompareTestTest1.json
    Renaming ./Base/Common/Testing/JsonCompareCommandTestBaseline1.json to ./Base/Common/Testing/JsonCompareTestBaseline1.json
    Renaming ./Base/Common/Testing/JsonCompareCommandTestBaseline2.json to ./Base/Common/Testing/JsonCompareTestBaseline2.json
    
    
    (2) Use of KFileReplace to change the strings within the files.
    
    
    (3) Configure and rebuild in a clean directory to confirm associated
    tests are still passing:
    
    $ ctest -R "(Image|Json|Text)Compare"
    Test project /home/jcfr/Projects/TubeTK-Refactor-BuildInITK-Release/TubeTK-build
          Start 15: ImageCompare-Test1_fetchData
     1/17 Test 15: ImageCompare-Test1_fetchData .....   Passed    0.00 sec
          Start 16: ImageCompare-Test1
     2/17 Test 16: ImageCompare-Test1 ...............   Passed    0.09 sec
          Start 17: ImageCompare-Test2_fetchData
     3/17 Test 17: ImageCompare-Test2_fetchData .....   Passed    0.00 sec
          Start 18: ImageCompare-Test2
     4/17 Test 18: ImageCompare-Test2 ...............   Passed    0.03 sec
          Start 19: ImageCompare-Test3_fetchData
     5/17 Test 19: ImageCompare-Test3_fetchData .....   Passed    0.00 sec
          Start 20: ImageCompare-Test3
     6/17 Test 20: ImageCompare-Test3 ...............   Passed    0.03 sec
          Start 21: ImageCompare-Test4_fetchData
     7/17 Test 21: ImageCompare-Test4_fetchData .....   Passed    0.00 sec
          Start 22: ImageCompare-Test4
     8/17 Test 22: ImageCompare-Test4 ...............   Passed    0.04 sec
          Start 23: ImageCompare-Test5_fetchData
     9/17 Test 23: ImageCompare-Test5_fetchData .....   Passed    0.00 sec
          Start 24: ImageCompare-Test5
    10/17 Test 24: ImageCompare-Test5 ...............   Passed    0.04 sec
          Start 25: ImageCompare-Test6fetchData
    11/17 Test 25: ImageCompare-Test6_fetchData .....   Passed    0.00 sec
          Start 26: ImageCompare-Test6
    12/17 Test 26: ImageCompare-Test6 ...............   Passed    0.02 sec
          Start 27: ImageCompare-Test7_fetchData
    13/17 Test 27: ImageCompare-Test7_fetchData .....   Passed    0.00 sec
          Start 28: ImageCompare-Test7
    14/17 Test 28: ImageCompare-Test7 ...............   Passed    0.02 sec
          Start 29: JsonCompare-Test1
    15/17 Test 29: JsonCompare-Test1 ................   Passed    0.00 sec
          Start 30: JsonCompare-Test2
    16/17 Test 30: JsonCompare-Test2 ................   Passed    0.00 sec
          Start 31: JsonCompare-Test3
    17/17 Test 31: JsonCompare-Test3 ................   Passed    0.00 sec

--- Base/CompareTools/TextCompare.cxx
+++ Base/CompareTools/TextCompare.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: TextCompareCommand.cxx, v $
+  Module:    $RCSfile: TextCompare.cxx, v $
   Language:  C++
   Date:      $Date: 2008-11-09 18:18:52 $
   Version:   $Revision: 1.12 $
