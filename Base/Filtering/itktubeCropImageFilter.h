InsightSoftwareConsortium/ITKTubeTK

commit d680b3f0221a6c9432a46eb34440e72c2a8cc3cd

STYLE: Move implementation of CropFilter  into TukeTK/Base
The ITKModules/ITKTubeTK directory contains now classes that
have an instance of the Base class they are shadowing.

Renames itktubeCropImage to tubeCropImage

To avoid changing the access modifier associated with method like PrintSelf,
itkTubeTK class are declared as friend within the corresponding base class.

As discussed with Stephen, the adopted convention is the following:

* Classes in TubeTK/Base will either be in "itk:tube" or "tube" namespace.

* Classes in TubeTK/ITKModules/ITKTubeTK will always be in "tube" namespace.

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

Base/Filtering/itktubeCropImageFilter.h
@@ -21,6 +21,13 @@
 #include "itkImage.h"
 #include "itkExtractImageFilter.h"
 
+// Forward declare itkTubeTK class to allow friendship
+namespace tube
+{
+  template< typename TInputImage, typename TOutputImage >
+  class CropImage;
+}
+
 namespace itk
 {
 namespace tube
@@ -118,6 +125,7 @@ public:
 protected:
   CropImageFilter( void );
   ~CropImageFilter() {}
+
   void PrintSelf(std::ostream & os, Indent indent) const ITK_OVERRIDE;
 
   void GenerateOutputInformation() ITK_OVERRIDE;
@@ -143,6 +151,10 @@ private:
   bool                           m_UseROIBoundary;
   float                          m_ProgressStart;
   float                          m_ProgressRange;
+
+  /** friendship facilitating itkTukeTK integration */
+  template< TInputImage, TOutputImage >
+  friend class ::tube::CropImage;
 };
 } // End namespace tube
 } // End namespace itk

Applications/CropImage/CropImage.cxx
@@ -20,11 +20,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 =========================================================================*/
+
+// TubeTK includes
 #include "tubeMessage.h"
 #include "tubeCLIProgressReporter.h"
 
-#include "itktubeCropImageFilter.h"
+// ITKTubeTK includes
+#include "tubeCropImage.h"
 
+// ITK includes
 #include <itkImageFileReader.h>
 #include <itkImageFileWriter.h>
 #include <itkImageRegionConstIterator.h>
@@ -96,7 +100,7 @@ int DoIt( int argc, char * argv[] )
 
     timeCollector.Start( "CropFilter" );
 
-    typedef itk::tube::CropImageFilter< ImageType, ImageType > CropFilterType;
+    typedef tube::CropImage< ImageType, ImageType > CropFilterType;
     typename CropFilterType::Pointer cropFilter = CropFilterType::New();
 
     cropFilter->SetInput( reader->GetOutput() );
@@ -242,7 +246,7 @@ int DoIt( int argc, char * argv[] )
       {
       //Create new instance of cropFilter each time we extract a portion of
       // the image to reset parameters
-      typedef itk::tube::CropImageFilter<ImageType, ImageType> CropFilterType;
+      typedef tube::CropImage<ImageType, ImageType> CropFilterType;
       typename CropFilterType::Pointer cropFilter = CropFilterType::New();
       cropFilter->SetInput( inputImage );
 

Base/Filtering/CMakeLists.txt
@@ -33,6 +33,7 @@ set( TubeTK_Base_Filtering_H_Files
   itktubeAnisotropicDiffusionTensorImageFilter.h
   itktubeAnisotropicEdgeEnhancementDiffusionImageFilter.h
   itktubeAnisotropicHybridDiffusionImageFilter.h
+  itktubeCropImageFilter.h
   itktubeExtractTubePointsSpatialObjectFilter.h
   itktubeFFTGaussianDerivativeIFFTFilter.h
   itktubeGaussianDerivativeFilter.h
@@ -56,6 +57,7 @@ set( TubeTK_Base_Filtering_HXX_Files
   itktubeAnisotropicDiffusionTensorImageFilter.hxx
   itktubeAnisotropicEdgeEnhancementDiffusionImageFilter.hxx
   itktubeAnisotropicHybridDiffusionImageFilter.hxx
+  itktubeCropImageFilter.hxx
   itktubeExtractTubePointsSpatialObjectFilter.hxx
   itktubeFFTGaussianDerivativeIFFTFilter.hxx
   itktubeGaussianDerivativeFilter.hxx

ITKModules/ITKTubeTK/include/tubeCropImage.h
@@ -1,0 +1,90 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+*=========================================================================*/
+#ifndef __tubeCropImage_h
+#define __tubeCropImage_h
+
+#include "itktubeCropImageFilter.h"
+#include "itkObject.h"
+
+
+namespace tube
+{
+/** \class CropImage
+ */
+
+template< typename TInputImage, typename TOutputImage >
+class CropImage:
+  public itk::Object
+{
+public:
+  /** Standard class typedefs. */
+  typedef CropImage                       Self;
+  typedef itk::SmartPointer< Self >       Pointer;
+  typedef itk::SmartPointer< const Self > ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(CropImage, Object);
+
+  typedef TInputImage ImageType;
+
+  void SetMin( typename ImageType::IndexType roiMin );
+
+  void SetMax( typename ImageType::IndexType roiMax );
+
+  void SetSize( typename ImageType::SizeType roiSize );
+
+  void SetCenter( typename ImageType::IndexType roiCenter );
+
+  void SetBoundary( typename ImageType::IndexType roiBoundary );
+
+  void SetMatchVolume( typename ImageType::ConstPointer matchVolume );
+
+  void SetMatchMask( typename ImageType::Pointer maskImage );
+
+  void SetSplitInput( typename ImageType::IndexType splitIndex,
+    typename ImageType::IndexType roiIndex );
+
+  void SetInput( const TInputImage *inputImage );
+  void Update();
+  typename TOutputImage::Pointer GetOutput();
+
+protected:
+  CropImage( void );
+  ~CropImage() {}
+  void PrintSelf(std::ostream & os, itk::Indent indent) const;
+
+private:
+  /** itkCropImageFilter parameters **/
+  CropImage(const Self &);
+  void operator=(const Self &);
+
+  typedef itk::tube::CropImageFilter< ImageType, ImageType > CropFilterType;
+  typename CropFilterType::Pointer m_CropFilter;
+
+};
+} // End namespace tube
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "tubeCropImage.hxx"
+#endif
+
+#endif // End !defined( __tubeCropImage_h )

ITKModules/ITKTubeTK/include/tubeCropImage.hxx
@@ -1,0 +1,133 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+*=========================================================================*/
+#ifndef __tubeCropImage_hxx
+#define __tubeCropImage_hxx
+
+#include "tubeCropImage.h"
+
+
+namespace tube
+{
+template< typename TInputImage, typename TOutputImage >
+CropImage< TInputImage, TOutputImage >
+::CropImage( void )
+{
+  m_CropFilter = CropFilterType::New();
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::SetMin( typename ImageType::IndexType roiMin )
+{
+  m_CropFilter->SetMin( roiMin );
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::SetMax( typename ImageType::IndexType roiMax )
+{
+  m_CropFilter->SetMax( roiMax );
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::SetSize( typename ImageType::SizeType roiSize )
+{
+  m_CropFilter->SetSize( roiSize );
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::SetCenter( typename ImageType::IndexType roiCenter )
+{
+  m_CropFilter->SetCenter( roiCenter );
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::SetBoundary( typename ImageType::IndexType roiBoundary )
+{
+  m_CropFilter->SetBoundary( roiBoundary );
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::SetMatchVolume( typename ImageType::ConstPointer matchVolume )
+{
+  m_CropFilter->SetMatchVolume( matchVolume );
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::SetMatchMask( typename ImageType::Pointer maskImage )
+{
+  m_CropFilter->SetMatchMask( maskImage );
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::SetSplitInput( typename ImageType::IndexType splitIndex,
+  typename ImageType::IndexType roiIndex )
+{
+  m_CropFilter->SetSplitInput( splitIndex, roiIndex );
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::SetInput( const TInputImage *inputImage )
+{
+  m_CropFilter->SetInput( inputImage );
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::Update()
+{
+  m_CropFilter->Update();
+}
+
+template< typename TInputImage, typename TOutputImage >
+typename TOutputImage::Pointer
+CropImage< TInputImage, TOutputImage >
+::GetOutput()
+{
+  return m_CropFilter->GetOutput();
+}
+
+template< typename TInputImage, typename TOutputImage >
+void
+CropImage< TInputImage, TOutputImage >
+::PrintSelf(std::ostream & os, itk::Indent indent) const
+{
+  m_CropFilter->PrintSelf( os, indent);
+}
+
+} // end namespace tube
+
+
+#endif

ITKModules/ITKTubeTK/wrapping/tubeCropImage.wrap
@@ -1,7 +1,7 @@
-itk_wrap_include( itktubeCropImageFilter.h )
+itk_wrap_include( tubeCropImage.h )
 itk_wrap_include( itkImage.h )
 
-itk_wrap_named_class("itk::tube::CropImageFilter" itktubeCropImageFilter POINTER)
+itk_wrap_named_class("tube::CropImage" tubeCropImage POINTER)
  foreach(d ${ITK_WRAP_IMAGE_DIMS})
     foreach(t ${WRAP_ITK_SCALAR})
       itk_wrap_template("I${ITKM_${t}}${d}I${ITKM_${t}}${d}"  "itk::Image<${ITKT_${t}}, ${d}>, itk::Image<${ITKT_${t}}, ${d}>")

commit f927159dbc9abb7fa3f3a10f415058c702ba7355

COMP: ITKTubeTK: Fix MacOSX build error related to friendship declaration.
This commit fixes a regression introduced in d680b3f (STYLE: Move
implementation of CropFilter  into TukeTK/Base)

Error fixed by this commit was happening when building target "CropImage":

  TubeTK/Base/Filtering/itktubeCropImageFilter.h:156:24: error: a non-type template parameter cannot have type 'itk::Image'

Base/Filtering/itktubeCropImageFilter.h
@@ -24,7 +24,7 @@
 // Forward declare itkTubeTK class to allow friendship
 namespace tube
 {
-  template< typename TInputImage, typename TOutputImage >
+  template< typename tube_TInputImage, typename tube_TOutputImage >
   class CropImage;
 }
 
@@ -153,7 +153,7 @@ private:
   float                          m_ProgressRange;
 
   /** friendship facilitating itkTukeTK integration */
-  template< TInputImage, TOutputImage >
+  template< typename tube_TInputImage, typename tube_TOutputImage >
   friend class ::tube::CropImage;
 };
 } // End namespace tube