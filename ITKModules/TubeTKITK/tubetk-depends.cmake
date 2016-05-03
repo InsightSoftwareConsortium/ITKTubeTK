af4bc7fae6a1084c1de7aa0f800f93825a5a4d14
Stephen Aylward <stephen.aylward@kitware.com>
2016-05-03 06:44:03 -0400

    STYLE: Fix line lengths

--- ITKModules/TubeTKITK/tubetk-depends.cmake
+++ ITKModules/TubeTKITK/tubetk-depends.cmake
@@ -2,14 +2,15 @@
 #
 # TubeTK Maintainers:
 #
-# TubeTK base components required to build TubeTKITK should be added to TubeTKITK_DEPENDS list.
+# TubeTK base components required to build TubeTKITK should be added to
+#   TubeTKITK_DEPENDS list.
 #
 # * Components should be listed in topological order.
 #
 # * Name should be specified without the 'TubeTK' prefix.
 #
-# * All components should be listed: This will ensure all required folder will be added
-# when building TubeTK as an ITK remote module.
+# * All components should be listed: This will ensure all required folder
+#     will be added when building TubeTK as an ITK remote module.
 #
 
 set( TubeTKITK_DEPENDS
