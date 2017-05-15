ca2fa0d4ab36713492432117474597351a8aa658
Neal Siekierski <neal.siekierski@kitware.com>
2017-05-15 17:25:35 -0400

    ComputeStatistics: sampling_roc

--- Examples/SegmentVesselsUsingNeuralNetworks/scripts-xyz-slices/ComputeStatistics.py
+++ Examples/SegmentVesselsUsingNeuralNetworks/scripts-xyz-slices/ComputeStatistics.py
@@ -2,6 +2,7 @@
 
 from glob import glob
 import os
+import random
 
 import itk
 import matplotlib
@@ -60,19 +61,57 @@ def whole_image_roc():
         network_im = itk.imread(str(os.path.join(test_output_dir, name + '_vess_prob.mha')))
         expert_arr, network_arr = map(itk.GetArrayViewFromImage, (expert_im, network_im))
         all_bins = np.bincount((256 * expert_arr + network_arr).reshape(-1), minlength=512).astype(float)
-        pairs = []
-        for i in range(257):
-            bins = np.array([all_bins[:i].sum(), all_bins[i:256].sum(),
-                             all_bins[256:i + 256].sum(), all_bins[i + 256:].sum()])
-            pairs.append((bins[1]/bins[:2].sum(), bins[3]/bins[2:].sum()))
-        pairs = np.stack(pairs, -1)
-        plt.figure()
-        plt.plot(pairs[0], pairs[1], 'k.-')
-        plt.savefig(os.path.join(base, name + '.png'))
+        write_roc_plot_from_all_bins(all_bins, os.path.join(base, name + '.png'))
+
+def sampling_roc(samples_per_class=5000):
+    """Sample so many positive and negative patches per image, and use the
+    model prediction on these to estimate the ROC curve.  Also
+    generate the confusion matrix using VESSEL_SEED_PROBABILITY as the
+    threshold for reference.
+
+    """
+    print("Generating sampled ROC curves")
+    base = os.path.join(stats_base, 'sampling_roc')
+    utils.ensureDirectoryExists(base)
+    w = script_params['PATCH_RADIUS']
+    model = utils.load_best_model()
+    name_keys = [os.path.basename(x)[:-12] for x in glob(os.path.join(test_data_dir, '*_prepped.mha'))]
+    for name in name_keys:
+        print(name)
+        expert_im = itk.imread(str(os.path.join(test_data_dir, name + '_expert.mha')))
+        prepped_im = itk.imread(str(os.path.join(test_data_dir, name + '_prepped.mha')))
+        expert_arr, prepped_arr = map(itk.GetArrayViewFromImage, (expert_im, prepped_im))
+        sample_ind = [random.sample(np.transpose(np.where(
+            (expert_arr if i else 1 - expert_arr)[(np.s_[w:-w],) * expert_arr.ndim]
+        )) + w, samples_per_class) for i in 0, 1]
+        neg_pred, pos_pred = (utils.predict_on_indices(
+            model, prepped_arr, sample_ind[0] + sample_ind[1],
+            script_params['DEPLOY_BATCH_SIZE'],
+        ).reshape((2, -1)) * 255).round().astype(int)
+        all_bins = np.concatenate((np.bincount(neg_pred, minlength=256),
+                                   np.bincount(pos_pred, minlength=256))).astype(float)
+        write_roc_plot_from_all_bins(all_bins, os.path.join(base, name + '.png'))
+        # TODO ROC curve
+
+def write_roc_plot_from_all_bins(all_bins, path):
+    """From a flat array of all expert-negative, the all expert-positive
+    bincounts, create an ROC plot
+
+    """
+    pairs = []
+    for i in range(257):
+        bins = np.array([all_bins[:i].sum(), all_bins[i:256].sum(),
+                         all_bins[256:i + 256].sum(), all_bins[i + 256:].sum()])
+        pairs.append((bins[1]/bins[:2].sum(), bins[3]/bins[2:].sum()))
+    pairs = np.stack(pairs, -1)
+    plt.figure()
+    plt.plot(pairs[0], pairs[1], 'k.-')
+    plt.savefig(path)
 
 def main():
     whole_image_confusion()
     whole_image_roc()
+    sampling_roc()
 
 if __name__ == '__main__':
     main()
