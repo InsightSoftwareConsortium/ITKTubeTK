f0548dd0dfcc3c2a554efc48693061907e051335
Neal Siekierski <neal.siekierski@kitware.com>
2017-05-17 10:37:34 -0400

    DeployCLI: Use ctk_cli for DeployCLI

--- Examples/SegmentVesselsUsingNeuralNetworks/scripts-xyz-slices/DeployCLI.py
+++ Examples/SegmentVesselsUsingNeuralNetworks/scripts-xyz-slices/DeployCLI.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python
 
-# TODO SEM CLI-ize
-
-import argparse
 import os.path
 
+import ctk_cli
 import keras.models as M
 
 import deploy
 import utils
 from utils import script_params
 
-parser = argparse.ArgumentParser(description='Segment vessels using a neural network')
-parser.add_argument('params', help='Path to the params.json file used to train the model')
-parser.add_argument('model', help='Path to a saved Keras model file')
-parser.add_argument('vascularModelFile', help='Path to file with vascular model parameters')
-parser.add_argument('inputImage')
-parser.add_argument('outputDir', nargs='?', default='.', help='Directory to write all output to.  Output file names all start with the basename of inputImage without its extension')
-parser.add_argument('--preprocessed', help='Path to an appropriately preprocessed version of inputImage.  Automatically generated if not given.')
+parser = ctk_cli.CLIArgumentParser()
 
 
 def main(args):
