3ae12c0d0309260c305b833f51c1ad802b6336a7
Neal Siekierski <neal.siekierski@kitware.com>
2017-06-12 15:09:39 -0400

    FlexibleParams: Teach params.py about some customizations

--- Examples/SegmentVesselsUsingNeuralNetworks/scripts-xyz-slices/params.py
+++ Examples/SegmentVesselsUsingNeuralNetworks/scripts-xyz-slices/params.py
@@ -1,14 +1,9 @@
+data_id = 'SVUNN'
+
 params = {
     "OUTPUT_DATA_ROOT": "/invalid/path/to/data",
-    "INPUT_DATA_ROOT": "/home/nealsiekierski/caffe/data/SegmentVesselsUsingNeuralNetworks",
-    "TYPE_SUBDIR_STRUCTURE": "*",
-    "TYPES": {
-        "Controls": "controls",
-        "LargeTumor": "tumors",
-    },
     "NETWORK_DESIGN": "xyz",
     "PATCH_RADIUS": 31,
-    "RESAMPLE_SPACING": null,
     "SMOOTHING_RADIUS": 2,
     "POSITIVE_PATCHES_PER_INPUT_FILE": 5000,
     "NEGATIVE_TO_POSITIVE_RATIO": 1.0,
@@ -26,5 +21,39 @@ params = {
     "WEIGHT_DECAY": 0.0001,
     "GAMMA": 0.02,
     "VESSEL_SEED_PROBABILITY": 0.95,
-    "VESSEL_SCALE": 0.1,
 }
+
+if data_id == 'SVUNN':
+    params.update({
+        "INPUT_DATA_ROOT": "/home/nealsiekierski/caffe/data/SegmentVesselsUsingNeuralNetworks",
+        "TYPE_SUBDIR_STRUCTURE": "*",
+        "TYPES": {
+            "Controls": "controls",
+            "LargeTumor": "tumors",
+        },
+        "RESAMPLE_SPACING": None,
+        "VESSEL_SCALE": 0.1,
+    })
+elif data_id == 'B-MRA':
+    params.update({
+        "INPUT_DATA_ROOT": "/home/nealsiekierski/data/Bullitt-MRA-converted/reflattened",
+        "TYPE_SUBDIR_STRUCTURE": "",
+        "TYPES": {
+            "Male": "male",
+            "Female": "female",
+        },
+        "RESAMPLE_SPACING": 0.513393,
+        "VESSEL_SCALE": 0.9,
+    })
+elif data_id == 'stroke':
+    params.update({
+        "INPUT_DATA_ROOT": "/home/nealsiekierski/data/Lee-StrokeCollaterals",
+        "TYPE_SUBDIR_STRUCTURE": "",
+        "TYPES": {
+            "restructured-with-skull": "data",
+        },
+        "RESAMPLE_SPACING": 0.5,
+        "VESSEL_SCALE": 0.9,
+    })
+else:
+    raise ValueError("Invalid value for data_id!")
