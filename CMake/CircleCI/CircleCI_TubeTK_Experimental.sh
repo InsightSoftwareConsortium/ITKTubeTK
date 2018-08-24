45aef87f48fa56ea34a97f9a812aab6af1beee35
Stephen Aylward <stephen@aylward.org>
2018-08-24 10:42:49 -0400

    BUG: Update repo name to itktubetk in circuleci config

--- CMake/CircleCI/CircleCI_TubeTK_Experimental.sh
+++ CMake/CircleCI/CircleCI_TubeTK_Experimental.sh
@@ -6,4 +6,4 @@ CTestCommand=ctest
 DashboardDir=/usr/src/
 
 cd ${DashboardDir}
-${CTestCommand} -D Experimental -D SITE_CTEST_MODE:STRING=Experimental -D SITE_BUILD_TYPE:STRING=${BuildType} -S /usr/src/TubeTK/CMake/CircleCI/CircleCI_$1_Docker.cmake -V -VV -O CircleCI_$1_Docker.log
+${CTestCommand} -D Experimental -D SITE_CTEST_MODE:STRING=Experimental -D SITE_BUILD_TYPE:STRING=${BuildType} -S /usr/src/ITKTubeTK/CMake/CircleCI/CircleCI_$1_Docker.cmake -V -VV -O CircleCI_$1_Docker.log
