346203298855d52f5e478ee0294ec02f7d836791
Stephen Aylward <aylward@krs-mbp.local>
2019-01-17 11:04:50 -0500

    COMP: Udate DashboardDate to initiate nightly CI builds

--- dashboards/dashboardDate.sh
+++ dashboards/dashboardDate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-
-echo `date` > .\dashboardDate.txt
-git checkout -b DashboardDate
+cd /Users/aylward/src/ITKTubeTK
+git checkout master --force
+git pull --all
+git checkout DashboardDate
+echo `date` > /Users/aylward/src/dashboards/ITKTubeTK/dashboards/dashboardDate.txt
 git commit dashboardDate.txt -m "Update DashboardDate to initiate night CI builds"
 git push origin dashboardDate
