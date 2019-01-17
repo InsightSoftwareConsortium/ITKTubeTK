e0384b73290ebc897a6ff19cbcad2029c923fb79
Stephen Aylward <stephen.aylward@kitware.com>
2019-01-17 12:26:44 -0500

    ENH: Script for updating date - run nightly on a cron machine

--- dashboards/dashboardDate.sh
+++ dashboards/dashboardDate.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-cd /Users/aylward/src/dashboards/ITKTubeTK/dashboards
-git checkout master --force
+rm -rf ITKTubeTK
+git clone git@github.com:/KitwareMedical/ITKTubeTK
+cd ITKTubeTK/dashboards
 git pull --all
 git checkout DashboardDate
 echo `date` > dashboardDate.txt
 git commit dashboardDate.txt -m "COMP: Update DashboardDate to initiate nightly CI builds"
-git push origin dashboardDate
+git push
