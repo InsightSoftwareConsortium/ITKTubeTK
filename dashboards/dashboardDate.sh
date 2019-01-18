776158566d9f33536596f5c6a1420d81513b8709
Stephen Aylward <stephen.aylward@kitware.com>
2019-01-18 15:44:29 -0500

    BUG: Was in incorrect directory for date update

--- dashboards/dashboardDate.sh
+++ dashboards/dashboardDate.sh
@@ -2,6 +2,7 @@
 cd /Users/aylward/src/dashboards/
 rm -rf ITKTubeTK
 git clone git@github.com:/KitwareMedical/ITKTubeTK
+cd ITKTubeTK/dashboards
 echo `date` > dashboardDate.txt
 git commit dashboardDate.txt -m "NIGHTLY: Update DashboardDate to initiate nightly CI builds"
 git push
