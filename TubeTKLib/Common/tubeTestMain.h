137db373761aa5291ed245cfbf4af9d6afbb2b27
Stephen Aylward <stephen@aylward.org>
2019-01-25 18:19:19 -0500

    BUG: USE_VTK for old VTK and some WorkUnits are still threads

--- TubeTKLib/Common/tubeTestMain.h
+++ TubeTKLib/Common/tubeTestMain.h
@@ -116,13 +116,13 @@ int main( int argc, char * argv[] )
       if( std::strcmp( argv[1], "--with-threads" ) == 0 )
         {
         int numThreads = std::atoi( argv[2] );
-        itk::MultiThreaderBase::SetGlobalDefaultNumberOfWorkUnits( numThreads );
+        itk::MultiThreaderBase::SetGlobalDefaultNumberOfThreads( numThreads );
         argv += 2;
         argc -= 2;
         }
       else if( std::strcmp( argv[1], "--without-threads" ) == 0 )
         {
-        itk::MultiThreaderBase::SetGlobalDefaultNumberOfWorkUnits( 1 );
+        itk::MultiThreaderBase::SetGlobalDefaultNumberOfThreads( 1 );
         argv += 1;
         argc -= 1;
         }
