d9588cec6fa1521d96cf2e021beccef4e137eff4
Stephen R. Aylward <stephen.aylward@kitware.com>
2019-09-30 22:54:27 -0400

    ENH: set numpy include path in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 from __future__ import print_function
 from os import sys
 
+import numpy as np
+
 try:
     from skbuild import setup
 except ImportError:
@@ -16,6 +18,7 @@ setup(
     version='0.1.0',
     author='Stephen R. Aylward',
     author_email='stephen.aylward@kitware.com',
+    include_dirs=[np.get_include()],
     packages=['itk'],
     package_dir={'itk': 'itk'},
     download_url=r'https://github.com/KitwareMedical/ITKTubeTK',
