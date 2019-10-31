fb8399f83dbbc5a8ccb521730bb0656d6c3fe524
Stephen R. Aylward <stephen.aylward@kitware.com>
2019-10-31 10:12:34 -0400

    BUG: Coverage task must be called after Test task for ctest to work

--- dashboards/dashboardDate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /Users/aylward/src/dashboards/
-rm -rf ITKTubeTK
-git clone git@github.com:/KitwareMedical/ITKTubeTK
-cd ITKTubeTK/dashboards
-echo `date` > dashboardDate.txt
-git commit dashboardDate.txt -m "NIGHTLY: Update DashboardDate to initiate nightly CI builds"
-git push
