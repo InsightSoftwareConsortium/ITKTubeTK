ad58b1807c8c3224b071df8f35775b0d98822cb2
Stephen Aylward <stephen.aylward@kitware.com>
2020-01-21 16:45:17 -0500

    COMP: Missing override keywords in include/tubeSegmentTubes.h

--- include/tubeSegmentTubes.h
+++ include/tubeSegmentTubes.h
@@ -165,7 +165,7 @@ public:
 protected:
   SegmentTubes( void );
   ~SegmentTubes() {}
-  void PrintSelf( std::ostream & os, itk::Indent indent ) const;
+  void PrintSelf( std::ostream & os, itk::Indent indent ) const override;
 
 private:
   /** itktubeTubeExtractor parameters **/
@@ -173,7 +173,8 @@ private:
   void operator=( const Self & );
 
   // To remove warning "was hidden [-Woverloaded-virtual]"
-  void SetInput( const DataObjectIdentifierType &, itk::DataObject * ) {};
+  void SetInput( const DataObjectIdentifierType &, itk::DataObject * ) 
+    override {};
 
   typename FilterType::Pointer m_Filter;
 
