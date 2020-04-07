8fadd4081499f564566e5823e364f72d62d18227
Stephen Aylward <stephen.aylward@kitware.com>
2020-04-07 19:09:38 -0400

    ENH: Update download script

--- examples/DownloadExampleData.sh
+++ examples/DownloadExampleData.sh
@@ -1,2 +1,2 @@
-#! /usr/bin/sh
-girder-client --api-url https://data.kitware.com/api/v1 localsync 58a3abaa8d777f0721a65b0a . 
+#! /bin/sh
+girder-client --api-url https://data.kitware.com/api/v1 localsync 58a3abaa8d777f0721a65b0a data
