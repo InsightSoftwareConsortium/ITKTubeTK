1bdf1c4fb96161adac724f07e8eb7f8fe1978a99
Hans Johnson <hans-johnson@uiowa.edu>
2025-01-26 12:57:59 -0600

    STYLE: CoordRepType -> CoordinateType code readability
    For the sake of code readability, a new 'CoordinateType' alias is added for
    each nested 'CoordRepType' alias. The old 'CoordRepType' aliases will still be
    available with ITK 6.0, but it is recommended to use 'CoordinateType' instead.
    The 'CoordRepType' aliases will be removed when 'ITK_FUTURE_LEGACY_REMOVE' is
    enabled. Similarly, 'InputCoordinateType', 'OutputCoordinateType', and
    'ImagePointCoordinateType' replace 'InputCoordRepType', 'OutputCoordRepType',
    and 'ImagePointCoordRepType', respectively.

--- src/Registration/itktubeMeanSquareRegistrationFunction.h
+++ src/Registration/itktubeMeanSquareRegistrationFunction.h
@@ -111,12 +111,12 @@ public:
   typedef typename Superclass::TimeStepType     TimeStepType;
 
   /** Interpolator type. */
-  typedef double                                     CoordRepType;
-  typedef InterpolateImageFunction<MovingImageType, CoordRepType>
+  typedef double                                     CoordinateType;
+  typedef InterpolateImageFunction<MovingImageType, CoordinateType>
                                                      InterpolatorType;
   typedef typename InterpolatorType::Pointer         InterpolatorPointer;
   typedef typename InterpolatorType::PointType       PointType;
-  typedef LinearInterpolateImageFunction<MovingImageType, CoordRepType>
+  typedef LinearInterpolateImageFunction<MovingImageType, CoordinateType>
                                                      DefaultInterpolatorType;
 
   /** Covariant vector type. */
